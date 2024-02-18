7b78d8b91d370bd47609cb060f1ad5b293a8fbb0
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-19 02:44:33 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.cpp
+++ 프로그래머스/0/181936. 공배수/공배수.cpp
@@ -4,9 +4,5 @@
 using namespace std;
 
 int solution(int number, int n, int m) {
-    int answer = 0;
-    if(number%n==0 && number%m==0)
-        answer = 1;
-    else answer = 0;
-    return answer;
+    return !(number%n) && !(number%m) ? 1:0;
 }
