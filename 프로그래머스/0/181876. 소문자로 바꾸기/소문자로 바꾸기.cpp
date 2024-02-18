30513163f50a1bdd34aab48655cacd3531dee10b
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-19 02:57:27 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(auto c : myString)
+        answer += tolower(c);
+    return answer;
+}
