3c925e71b7a766e1deaab84170bed1ad7be98d81
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-19 02:24:07 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cpp
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cpp
@@ -6,13 +6,12 @@ using namespace std;
 int solution(vector<int> num_list) {
     int answer = 0;
     if(num_list.size()>=11){
-        for(int i=0;i<num_list.size();i++){
-            answer += num_list[i];
-        }
+        for(auto c : num_list)
+            answer += c;
     } else {
-        answer = num_list[0];
-        for(int i=1;i<num_list.size();i++){
-            answer *= num_list[i];
+        answer = 1;
+        for(auto c : num_list){
+            answer *= c;
         }
     }
     return answer;
