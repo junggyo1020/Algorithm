1013cc3e2de5ff1f7024debda5c7d984fa4ba531
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-19 01:05:33 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 string solution(string my_string, string overwrite_string, int s) {
     string answer = "";
-    answer = my_string.substr(0,s) + overwrite_string 
-        + my_string.substr(s+overwrite_string.length());
+    answer = my_string.replace(s,overwrite_string.length(),overwrite_string);
     return answer;
 }
