58647823c0a2c31025eaa597db12179516ecacf4
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-19 14:31:58 +0900

    [level 0] Title: n의 배수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.cpp
+++ 프로그래머스/0/181937. n의 배수/n의 배수.cpp
@@ -4,9 +4,5 @@
 using namespace std;
 
 int solution(int num, int n) {
-    int answer = 0;
-    if(!(num%n))
-        answer = 1;
-    else answer = 0;
-    return answer;
+    return num%n == 0;
 }
