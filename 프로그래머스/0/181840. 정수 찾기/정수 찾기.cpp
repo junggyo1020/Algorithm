c36bf42f52d904d32ee7050058d306f45a4dcc4c
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-19 14:36:24 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -7,10 +7,8 @@ int solution(vector<int> num_list, int n) {
     int answer = 0;
     for(auto c : num_list){
         if(c==n){
-            answer=1;
-            break;
+            return 1;
         }
-        else answer=0;
     }
-    return answer;
+    return 0;
 }
