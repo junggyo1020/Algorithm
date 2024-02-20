532dbc596987b04d59c1ff4965230a2884cb39c7
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-20 17:55:50 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cpp
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cpp
@@ -4,9 +4,6 @@
 using namespace std;
 
 vector<int> solution(vector<int> num_list, int n) {
-    vector<int> answer;
-    for(int i=0;i<n;i++){
-        answer.push_back(num_list[i]);
-    }
+    vector<int> answer(num_list.begin(),num_list.begin()+n);
     return answer;
 }
