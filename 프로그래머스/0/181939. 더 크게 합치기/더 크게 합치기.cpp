6b52c65d1e30d26399eaa14de3c7b4f845c69386
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-20 16:18:30 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -5,7 +5,5 @@
 using namespace std;
 
 int solution(int a, int b) {
-    int num1 = stoi(to_string(a) + to_string(b));
-    int num2 = stoi(to_string(b) + to_string(a));
-    return num1>num2 ? num1:num2;
+    return max(stoi(to_string(a) + to_string(b)),stoi(to_string(b) + to_string(a)));
 }
