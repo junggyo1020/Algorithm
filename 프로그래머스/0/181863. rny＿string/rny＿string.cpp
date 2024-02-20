52e0954529501a4b146bbf3834c2a5b4fd07a7bf
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-20 17:43:43 +0900

    [level 0] Title: rny_string, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.cpp
+++ 프로그래머스/0/181863. rny＿string/rny＿string.cpp
@@ -1,13 +1,16 @@
+#include <bits/stdc++.h>
 #include <string>
 #include <vector>
+#include <regex>
  //단일 문자열과 문자열의 입력 추가 방식 차이 구분
 using namespace std;
 
 string solution(string rny_string) {
     string answer = "";
-    for(auto& c:rny_string){
-        if( c=='m' ) answer += "rn";
-        else answer.push_back(c);
-    }
+    answer = regex_replace(rny_string,regex("m"),"rn");
+    // for(auto& c:rny_string){
+    //     if( c=='m' ) answer += "rn";
+    //     else answer.push_back(c);
+    // }
     return answer;
 }
