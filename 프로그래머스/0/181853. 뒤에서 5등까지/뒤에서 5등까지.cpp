64790b417f9445461f1b05ad27a655a3d1192ea6
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-22 18:05:54 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    sort(num_list.begin(),num_list.end());
+    vector<int> answer;
+    for(int i=0;i<5;i++){
+        answer.push_back(num_list[i]);
+    }
+    return answer;
+}
