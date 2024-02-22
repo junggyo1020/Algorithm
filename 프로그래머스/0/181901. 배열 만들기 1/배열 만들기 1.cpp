bffdfb5276578afb0a2c11ce6fa20272f0b453a2
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-22 23:11:03 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.33 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cpp
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<int> solution(int n, int k) {
     vector<int> answer;
     for(int i=1;i<=n;i++){
-        if(!(i%k)) answer.push_back(i);
+        if(!(i%k)) answer.emplace_back(i);
     }
     
     return answer;
