9876883d3f913f2c82dba44dddb3751a5a3ef7bc
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-02-26 16:25:05 +0900

    [level 0] Title: 접미사 배열, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    for(int i=0;i<my_string.length();i++){
+        answer.push_back(my_string.substr(i));
+    }
+    sort(answer.begin(),answer.end());
+    return answer;
+}
