92c5452ce26c8c4e0ad17c2024f64bd3809f4d0a
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-03-01 17:54:10 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2741                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: jungyo1020 <boj.kr/u/jungyo1020>            +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2741                           #+#        #+#      #+#    */
+/*   Solved: 2024/03/01 17:49:56 by jungyo1020    ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
