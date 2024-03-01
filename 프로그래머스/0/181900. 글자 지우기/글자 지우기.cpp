5007de7cdca6761e2b43bc408af5d15030dda12f
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-03-01 17:37:02 +0900

    [level 0] Title: 글자 지우기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, vector<int> indices) {
+    string answer = "";
+    for(int i=0;i<my_string.length();i++){
+        if(find(indices.begin(),indices.end(),i) == indices.end()){
+            answer += my_string[i];
+        }
+    }
+    return answer;
+}
