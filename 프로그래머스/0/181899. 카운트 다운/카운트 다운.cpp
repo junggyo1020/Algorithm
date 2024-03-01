ad8db4249b7d6e09965774adc2efe29c7deef0c3
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-03-01 17:45:25 +0900

    [level 0] Title: 카운트 다운, Time: 0.01 ms, Memory: 4.24 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.cpp
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.cpp
@@ -5,13 +5,8 @@ using namespace std;
 
 vector<int> solution(int start, int end_num) {
     vector<int> answer;
-    int cnt = start;
-    while(1){
-        if(cnt==end_num){
-            answer.push_back(cnt); break;   
-        }
-        answer.push_back(cnt);
-        cnt--;
+    for(int i=start;i>=end_num;i--){
+        answer.emplace_back(i);
     }
     return answer;
 }
