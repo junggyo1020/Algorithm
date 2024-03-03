03df5d9e1e304fb3e22875819f32b04536bcd2f5
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-03-03 22:10:52 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,55 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 15829                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: jungyo1020 <boj.kr/u/jungyo1020>            +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/15829                          #+#        #+#      #+#    */
-/*   Solved: 2024/03/03 16:46:58 by jungyo1020    ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
-/**
- * 입력은 영문 소문자로만 이루어짐
- * a-z는 각각 1-26까지 고유번호로 부여
- * 문자열을 하나의 수열로 만들어야 한다.
- * 문자열의 각각 문자의 고유번호를 모두 더한 후 적당히 큰 수 M으로 나눠보자. -> 이 값이 해시 값
- * 하지만 이경우 문자열 순서만 바뀌어도 해쉬값의 충돌이 일어난다. -> 수정하자!
- * 각 항의 번호만큼 특정 숫자를 거듭제곱해서 곱해주도록 해쉬값을 지정해보자
- */
-
 #include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
-#include <cmath>
 #define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define endl "\n"
 using namespace std;
 
+const long long M = 1234567891;
+const long long base = 31;
+
 int L;
 string s;
-int idx;
-int sum = 0;
 
 void input()
 {
     cin >> L >> s;
 }
 
-void doHash()
+long long doHash()
 {
+    long long hashValue = 0;
+    long long powValue = 1; // 31^0 = 1에서 시작
+
     for (int i = 0; i < L; i++)
     {
-        idx = s[i] - 'a' + 1; // 고유번호 부여
-        sum += idx * pow(31, i);
+        int idx = s[i] - 'a' + 1; // 고유번호 부여, 'a'는 1
+        hashValue = (hashValue + (idx * powValue) % M) % M; // 해시 값 계산
+
+        powValue = (powValue * base) % M; // 다음 거듭제곱 값을 계산
     }
-    int result = sum % 1234567891;
-    cout << result << endl;
+
+    return hashValue;
 }
 
 int main()
 {
+    fastio;
     input();
-    doHash();
+    cout << doHash() << endl;
 }
