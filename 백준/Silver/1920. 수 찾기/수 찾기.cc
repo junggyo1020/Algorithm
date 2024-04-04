14f56820ae64e37aa00fdffd9ccbe992acb908d0
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-04-05 00:23:21 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -6,30 +6,23 @@
 /*   By: jungyo1020 <boj.kr/u/jungyo1020>            +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/1920                           #+#        #+#      #+#    */
-/*   Solved: 2024/03/04 16:52:10 by jungyo1020    ###          ###   ##.kr    */
+/*   Solved: 2024/04/04 23:53:44 by jungyo1020    ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-#define endl "\n"
+#include <bits/stdc++.h>
 using namespace std;
-
-int binarySearch(const vector<int> &arr, int target)
+int find(const vector<int> &v, int target)
 {
     int low = 0;
-    int high = arr.size() - 1;
-
+    int high = v.size() - 1;
     while (low <= high)
     {
         int mid = (low + high) / 2;
-        if (arr[mid] == target)
+        if (v[mid] == target)
         {
             return 1;
         }
-        else if (arr[mid] < target)
+        else if (v[mid] < target)
         {
             low = mid + 1;
         }
@@ -38,33 +31,31 @@ int binarySearch(const vector<int> &arr, int target)
             high = mid - 1;
         }
     }
-
-    return 0; // 타겟을 찾지 못했다면 0전환
+    return 0;
 }
-
 int main()
 {
-    fastio;
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
     int n, m;
     cin >> n;
-
-    vector<int> arr(n);
+    vector<int> v(n);
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        cin >> v[i];
     }
 
-    sort(arr.begin(), arr.end());
+    sort(v.begin(), v.end());
 
     cin >> m;
-    vector<int> arr2(m);
+    vector<int> v2(m);
     for (int i = 0; i < m; i++)
     {
-        cin >> arr2[i];
+        cin >> v2[i];
     }
 
     for (int i = 0; i < m; i++)
     {
-        cout << binarySearch(arr, arr2[i]) << endl;
+        cout << find(v, v2[i]) << '\n';
     }
 }
