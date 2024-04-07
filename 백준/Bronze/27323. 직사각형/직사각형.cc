d6d5a45e42c2f1ec1ccf5241af638472ae5ca2ef
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-04-07 23:39:17 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,9 @@
+//정수 A, B 가 주어진다. 세로 길이가 A cm, 가로 길이가 B cm 인 아래와 같은 직사각형의 넓이를 cm2 단위로 구하시오.
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    cout << a*b << '\n';
+    return 0;
+}
