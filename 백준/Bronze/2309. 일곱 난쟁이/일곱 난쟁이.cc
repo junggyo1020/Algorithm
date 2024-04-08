ceffd25812b8e23ee636f9ed8895aefc36a52de9
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-04-08 23:59:08 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,35 +1,44 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 2309                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: jungyo1020 <boj.kr/u/jungyo1020>            +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/2309                           #+#        #+#      #+#    */
-/*   Solved: 2024/03/11 20:23:43 by jungyo1020    ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
 #include <bits/stdc++.h>
 #define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
-int a[9];
+int height[9];
+int sum = 0;
+vector<int> v;
+pair<int, int> ret;
+void solve()
+{
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (sum - height[i] - height[j] == 100)
+            {
+                ret = {i, j};
+                return;
+            }
+        }
+    }
+}
+
 int main()
 {
     fastio;
     for (int i = 0; i < 9; i++)
     {
-        cin >> a[i];
+        cin >> height[i];
+        sum += height[i];
     }
-    sort(a, a + 9);
-    do
+    solve();
+    for (int i = 0; i < 9; i++)
     {
-        int sum = 0;
-        for (int i = 0; i < 7; i++)
-            sum += a[i];
-        if (sum == 100)
-            break;
-    } while (next_permutation(a, a + 9));
-    for (int i = 0; i < 7; i++)
-        cout << a[i] << "\n";
+        if (ret.first == i || ret.second == i)
+            continue;
+        v.push_back(height[i]);
+    }
+    sort(v.begin(), v.end());
+    for (int i : v)
+    {
+        cout << i << '\n';
+    }
     return 0;
 }
