978df67bca3f97e53e2ea7e29ec42f83139dfbb5
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-04-12 16:23:32 +0900

    [Silver III] Title: 통계학, Time: 72 ms, Memory: 4132 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -1,44 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2108                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: jungyo1020 <boj.kr/u/jungyo1020>            +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2108                           #+#        #+#      #+#    */
+/*   Solved: 2024/04/11 16:49:38 by jungyo1020    ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
 #include <iostream>
-#include <algorithm>
+#include <algorithm> // max_element
 #include <vector>
-#include <cmath>
+#include <numeric> //accumulate
+#include <cmath>   //반올림
+#include <iomanip> // 반올림
+#define endl "\n"
 using namespace std;
-vector<int> arr;
-int main() {
-    int num,tmp,range,middle = 0,most_val,mean = 0;
-    int most = -9999;
-    int number[8001] = {0,};
-    bool not_first = false;
-    cin >> num;
-    for(int i = 0; i < num; i++)
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
     {
-        cin >> tmp;
-        arr.push_back(tmp);
-        mean += tmp;
-        number[tmp+4000]++;
+        cin >> v[i];
     }
-    sort(arr.begin(),arr.end());
-    for(int i = 0; i < 8001; i++)
+    // 1. 산술평균
+    double sum = accumulate(v.begin(), v.end(), 0.0);
+    double avg = sum / n;
+    avg = round(avg);
+    // -0을 0으로 조정
+    if (avg == -0.0)
     {
-        if(number[i] == 0)
-            continue;
-        if(number[i] == most)
+        avg = 0.0;
+    }
+    cout << avg << "\n";
+    // 2. 중앙값
+    sort(v.begin(), v.end());
+    cout << v[n / 2] << endl;
+    // 3. 최빈값
+    // 빈도수를 저장하는 벡터 추가
+    vector<int> freq(8001, 0);
+    for (int num : v)
+    {
+        freq[num + 4000]++;
+    }
+    int max_freq = *max_element(freq.begin(), freq.end());
+    // 최대 빈도수인 값을 모두 저장
+    vector<int> modes;
+    for (int i = 0; i < freq.size(); i++)
+    {
+        if (freq[i] == max_freq)
         {
-            if(not_first)
-            {
-                most_val = i - 4000;
-                not_first = false;
-            }
-        }
-        if(number[i] > most)
-        {
-            most = number[i];
-            most_val = i - 4000;
-            not_first = true;
+            modes.push_back(i - 4000);
         }
     }
-    middle = arr[arr.size()/2];
-    mean = round((float)mean / num);
-    range = arr.back() - arr.front();
-    cout << mean << '\n' << middle << '\n' << most_val << '\n' << range;
+    sort(modes.begin(), modes.end());
+    cout << (modes.size() > 1 ? modes[1] : modes[0]) << endl;
+    // 4. 범위
+    cout << v.back() - v.front() << "\n";
+
+    return 0;
 }
