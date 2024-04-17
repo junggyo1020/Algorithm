54217e2b4a3a7d9a76eff0f0a20b89e0332cade3
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-04-17 22:35:38 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,19 @@
+//백준11653 소인수분해
+ 
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int N;
+    cin >> N;
+ 
+    if (N == 1) return 0;
+ 
+    for (int i = 2; i <= N; i++) {
+        while (N % i == 0) {
+            cout << i << endl;
+            N /= i;
+        }
+    }
+ 
+}
