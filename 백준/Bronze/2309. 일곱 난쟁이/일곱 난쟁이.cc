0ab65a3d9182a537153f90e0137629e9249aa3ea
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-07-25 01:04:25 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
-#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
-int height[9];
-int sum = 0;
-vector<int> v;
+int a[9], sum;
 pair<int, int> ret;
+vector<int> v;
 void solve()
 {
     for (int i = 0; i < 9; i++)
     {
         for (int j = 0; j < i; j++)
         {
-            if (sum - height[i] - height[j] == 100)
+            if (sum - a[i] - a[j] == 100)
             {
                 ret = {i, j};
                 return;
@@ -22,23 +20,20 @@ void solve()
 
 int main()
 {
-    fastio;
     for (int i = 0; i < 9; i++)
     {
-        cin >> height[i];
-        sum += height[i];
+        cin >> a[i];
+        sum += a[i];
     }
     solve();
     for (int i = 0; i < 9; i++)
     {
         if (ret.first == i || ret.second == i)
             continue;
-        v.push_back(height[i]);
+        v.push_back(a[i]);
     }
     sort(v.begin(), v.end());
     for (int i : v)
-    {
-        cout << i << '\n';
-    }
+        cout << i << "\n";
     return 0;
 }
