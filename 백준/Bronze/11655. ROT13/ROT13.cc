e6b50030150f1eb07fc10d8f927f2382b1baf707
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-07-25 23:30:15 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,34 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 11655                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: jungyo1020 <boj.kr/u/jungyo1020>            +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/11655                          #+#        #+#      #+#    */
-/*   Solved: 2024/03/17 16:32:04 by jungyo1020    ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
 #include <bits/stdc++.h>
 using namespace std;
-string s;
 int main()
 {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    string s;
     getline(cin, s);
     for (int i = 0; i < s.length(); i++)
     {
-        if ('a' <= s[i] && s[i] <= 'z')
+        // 공백
+        if (s[i] == ' ')
         {
-            s[i] = (s[i] - 'a' + 13) % 26 + 'a';
+            continue;
         }
-        else if ('A' <= s[i] && s[i] <= 'Z')
+        // 대문자
+        else if (s[i] >= 'A' && s[i] <= 'Z')
         {
             s[i] = (s[i] - 'A' + 13) % 26 + 'A';
         }
+        // 소문자
+        else if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] = (s[i] - 'a' + 13) % 26 + 'a';
+        }
     }
-    for (int i = 0; i < s.length(); i++)
+    for (char ch : s)
     {
-        cout << s[i];
+        cout << ch;
     }
 }
