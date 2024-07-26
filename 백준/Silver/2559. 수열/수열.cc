4dd8ec6179931bd4e16261da96763cdef3f3ef1e
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-07-26 21:01:10 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,31 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 2559                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: jungyo1020 <boj.kr/u/jungyo1020>            +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/2559                           #+#        #+#      #+#    */
-/*   Solved: 2024/03/17 20:41:26 by jungyo1020    ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int n, k, temp, psum[100001], ret = -10000004;
+int psum[100004];
+int ret = -10000004;
 int main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    // n과 k값 입력
+    int n, k;
     cin >> n >> k;
+    // 누적합 구하기
     for (int i = 1; i <= n; i++)
     {
+        int temp;
         cin >> temp;
         psum[i] = psum[i - 1] + temp;
     }
+    // 누적합 중 최대값 뽑기
     for (int i = k; i <= n; i++)
     {
         ret = max(ret, psum[i] - psum[i - k]);
     }
-    cout << ret << '\n';
+    cout << ret << "\n";
     return 0;
 }
