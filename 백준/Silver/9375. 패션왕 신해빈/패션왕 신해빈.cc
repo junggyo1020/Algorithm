d3a8a41750676de551384af83829f40c29171331
Willy <150939763+junggyo1020@users.noreply.github.com>
2024-07-29 08:46:46 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,43 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 9375                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: jungyo1020 <boj.kr/u/jungyo1020>            +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/9375                           #+#        #+#      #+#    */
-/*   Solved: 2024/03/20 12:56:25 by jungyo1020    ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * 입력 처리: 각 케이스에 대해 의상의 수와 각 의상의 이름 및 종류를 입력받습니다.
+ * 종류별 의상 수 계산: 각 종류별 의상의 개수를 세어서 저장.
+ * 조합 계산: 각 종류별로 (해당 종류의 의상 수 + 1)을 모두 곱한 후, 아무것도 입지 않는 경우를 뺸다.
+ */
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int t, n;
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t; // 테스트 케이스 수
+    int n; // 의상의 수
+
     cin >> t;
+
     while (t--)
     {
         cin >> n;
-        map<string, int> clothes;
+
+        unordered_map<string, int> clothes; // 각 종류별 의상의 개수
+
         for (int i = 0; i < n; i++)
         {
-            string name, category;
-            cin >> name >> category;
-            clothes[category]++;
+            string name, type;
+            cin >> name >> type;
+            clothes[type]++;
         }
 
-        int combinations = 1;
-        for (auto it = clothes.begin(); it != clothes.end(); ++it)
+        int result = 1; // 경우의 수    
+        for (auto &c : clothes)
         {
-            combinations *= (it->second + 1);
+            result *= (c.second + 1);
         }
 
-        // 아무것도 입지 않는 경우를 제외
-        cout << combinations - 1 << '\n';
+        cout << result - 1 << "\n";
     }
     return 0;
 }
