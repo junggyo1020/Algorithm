d61f1b3534866386ad7bb190f49acb2a61bf9fc3
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-08-18 15:14:54 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -6,45 +6,42 @@
 /*   By: jungyo1020 <boj.kr/u/jungyo1020>            +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/9012                           #+#        #+#      #+#    */
-/*   Solved: 2024/03/05 16:42:26 by jungyo1020    ###          ###   ##.kr    */
+/*   Solved: 2024/08/18 14:51:26 by jungyo1020    ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
-#include <iostream>
-#include <stack>
-#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-#define endl "\n"
+#include <bits/stdc++.h>
 using namespace std;
-
-string isValid(string s)
-{
-    stack<char> st;
-    for (char c : s)
-    {
-        // '('를 넣어야 하는 경우
-        if (c == '(')
-        {
-            st.push(c); // 일단 스택에 넣음
-        }
-        else
-        { // ')'를 스택에 넣어야하는 경우
-            if (st.empty())
-            { //')'를 입력받는 경우에 stack이 비어있는 경우 ))이런 경우 VPS(X)
-                return "NO";
-            }
-            st.pop();
-        }
-    }
-    return st.empty() ? "YES" : "NO";
-}
-
+int t;
+string result;
 int main()
 {
-    int T;
-    cin >> T;
-    while (T--)
+    cin >> t;
+    while (t--)
     {
-        string s;
+        string s = "";
+        stack<char> st;
         cin >> s;
-        cout << isValid(s) << endl;
+        for (char ch : s)
+        {
+            if (ch == '(')
+            {
+                st.push('(');
+            }
+            else
+            {
+                // st.top()을 빈 스택에서 호출할 경우 런타임 에러가 발생한다.
+                if (!st.empty() && st.top() == '(')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(')');
+                }
+            }
+        }
+        // 스택이 비어있으면 YES, 아니면 NO 출력
+        result = st.empty() ? "YES" : "NO";
+        cout << result << '\n';
     }
 }
