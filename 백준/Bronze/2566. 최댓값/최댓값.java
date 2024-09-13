e0da2ebb89579760cce9eabc8011db6b882a6233
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-09-13 20:50:27 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 16140 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -4,10 +4,9 @@ class Main{
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int[][] arr = new int[9][9];
+        
         int max_idx_x = -1;
         int max_idx_y = -1;
         int max_val = -1;
