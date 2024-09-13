30aa8c564803916f62ebfbb06b75783fed960c0d
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-09-13 20:30:13 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 176 ms, Memory: 17616 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -12,7 +12,6 @@ class Main{
         int m = Integer.parseInt(input[1]);
 
         int[][] arr = new int[n][m];
-        int[][] arr2 = new int[n][m];
         for (int i = 0; i < n; i++) {
             String[] s1 = br.readLine().split(" ");
             for (int j = 0; j < m; j++) {
@@ -22,13 +21,13 @@ class Main{
         for (int i = 0; i < n; i++) {
             String[] s2 = br.readLine().split(" ");
             for (int j = 0; j < m; j++) {
-                arr2[i][j] = Integer.parseInt(s2[j]);
+                arr[i][j] += Integer.parseInt(s2[j]);
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                bw.write(arr[i][j] + arr2[i][j] + " ");
+                bw.write(arr[i][j] + " ");
             }
             bw.write("\n");
         }
