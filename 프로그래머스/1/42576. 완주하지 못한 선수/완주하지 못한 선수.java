3933e53626e0260c85245181e09959a0cf39811a
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-09-21 17:36:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.76 ms, Memory: 97.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String str : participant){
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        for(String str2 : completion){
+            map.put(str2, map.get(str2) - 1);
+        }
+        
+        Iterator<Map.Entry<String,Integer>> iter = map.entrySet().iterator();
+        
+        while(iter.hasNext()){
+            Map.Entry<String, Integer> entry = iter.next();
+            if(entry.getValue()!=0){
+                answer = entry.getKey();
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
