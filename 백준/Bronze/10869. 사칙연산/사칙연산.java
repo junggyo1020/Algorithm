9dcc32fbcbd9d7d1150e9337a37e7f405f2e9e78
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-09-28 23:52:45 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -4,13 +4,12 @@ class Main{
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a % b);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
     }
 }
