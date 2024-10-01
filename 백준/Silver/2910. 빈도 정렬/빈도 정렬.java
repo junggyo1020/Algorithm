756c73717eb3ffedc98dc4fdafffa5bacdbecec5
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-10-01 15:51:59 +0900

    [Silver III] Title: 빈도 정렬, Time: 152 ms, Memory: 15964 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -1,30 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+class Main {
 
-    static HashMap<Integer, Integer> map = new LinkedHashMap<>();
-    static int n,c;
+    static int n, c;
+    static HashMap<Integer, Integer> map = new LinkedHashMap<>(); // 순서를 보존하기 위해 LinkedHashMap 선택
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        c = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int c = Integer.parseInt(input[1]);
 
+        String[] input2 = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            int input = sc.nextInt();
-            map.put(input, map.getOrDefault(input, 0 ) + 1);
+            int temp = Integer.parseInt(input2[i]);
+            map.put(temp, map.getOrDefault(temp, 0) + 1);
         }
 
         List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
 
-        list.sort((a,b) -> {
-            if(b.getValue().equals(a.getValue())){
-                return 0;
-            } else {
-                return b.getValue() - a.getValue();
-            }
+        list.sort((a, b) -> {
+            return b.getValue() - a.getValue(); // 내림차순 정렬
         });
 
+        // 결과 출력
         for(Map.Entry<Integer, Integer> entry : list){
             int num = entry.getKey();
             int freq = entry.getValue();
