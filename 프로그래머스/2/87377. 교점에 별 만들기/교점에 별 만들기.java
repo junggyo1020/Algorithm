8002420ce1ee526bf6a5add799865a912c145aa6
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-03 14:46:20 +0900

    [level 2] Title: 교점에 별 만들기, Time: 32.08 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,10 +1,9 @@
 import java.util.*;
-
 class Solution {
     
     private static class Point{
-        public final long x;
-        public final long y;
+        
+        public final long x,y;
         
         Point(long x, long y){
             this.x = x;
@@ -12,26 +11,15 @@ class Solution {
         }
     }
     
+    //정수인 교점만 구하기
     private Point intersection(long A, long B, long E, long C, long D, long F){
-        double x = (double)((B*F) - (E*D)) / ((A*D) - (B*C));
-        double y = (double)((E*C) - (A*F)) / ((A*D) - (B*C));
         
-        // 정수로만 표현되는 좌표만 입력하기
-        if(x % 1 != 0 || y % 1 != 0) return null;
+        double x = (double)((B*F)-(E*D))/((A*D)-(B*C));
+        double y = (double)((E*C)-(A*F))/((A*D)-(B*C));
+        
+        if(x%1!=0||y%1!=0) return null;
         
         return new Point((long) x, (long) y);
-    }
-    
-    private Point getMinimumPoint(List<Point> points){
-        long x = Long.MAX_VALUE;
-        long y = Long.MAX_VALUE;
-        
-        for(Point p : points){
-            if(p.x < x) x = p.x;
-            if(p.y < y) y = p.y;
-        }
-        
-        return new Point(x, y);
     }
     
     private Point getMaximumPoint(List<Point> points){
@@ -43,7 +31,19 @@ class Solution {
             if(p.y > y) y = p.y;
         }
         
-        return new Point(x, y);
+        return new Point(x,y);
+    }
+    
+    private Point getMinimumPoint(List<Point> points){
+        long x = Long.MAX_VALUE;
+        long y = Long.MAX_VALUE;
+        
+        for(Point p : points){
+            if(p.x < x) x = p.x;
+            if(p.y < y) y = p.y;
+        }
+        
+        return new Point(x,y);
     }
     
     public String[] solution(int[][] line) {
@@ -53,16 +53,14 @@ class Solution {
                 Point intersection = intersection(line[i][0], line[i][1], line[i][2],
                                                   line[j][0], line[j][1], line[j][2]);
                 
-                // 교점이 있을 경우 리스트에 저장
                 if(intersection != null){
                     points.add(intersection);
                 }
             }
         }
         
-        // minimum과 maximum 포인트를 찾기
-        Point minimum = getMinimumPoint(points);
         Point maximum = getMaximumPoint(points);
+        Point minimum = getMinimumPoint(points);
         
         int width = (int)(maximum.x - minimum.x + 1);
         int height = (int)(maximum.y - minimum.y + 1);
@@ -75,8 +73,10 @@ class Solution {
         for(Point p : points){
             int x = (int)(p.x - minimum.x);
             int y = (int)(maximum.y - p.y);
+            
             arr[y][x] = '*';
         }
+        
         
         String[] result = new String[arr.length];
         for(int i = 0; i < result.length; i++){
