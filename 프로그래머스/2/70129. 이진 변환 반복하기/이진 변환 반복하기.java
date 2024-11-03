78453dc49e8dbfaeb842f4221bfd8aabe1d782c7
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-03 19:45:41 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.89 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -3,28 +3,22 @@ class Solution {
     private int countZeros(String s){
         int zeros = 0;
         for(char c : s.toCharArray()){
-            if(c == '0'){
-                zeros++;
-            }
+            if(c == '0') zeros++;
         }
         return zeros;
     }
     
     public int[] solution(String s) {
-        int loop = 0; // 반복 횟수
-        int removed = 0; // 제거할 0의 개수
-        // 문자열 s가 "1"이 될 때까지 반복한다.
+        int zero = 0;
+        int loop = 0;
         while(!s.equals("1")){
-            // 문자열 s의 모든 0을 제거한다.
             int zeros = countZeros(s);
             loop += 1;
-            removed += zeros;
+            zero += zeros;
             
-            // 문자열 s의 길이 c를 2진법으로 표현한 문자열로 변환
             int ones = s.length() - zeros;
             s = Integer.toString(ones, 2);
         }
-        // 변환된 횟수와 제거된 모든 0의 개수를 배열에 담아 반환한다.
-        return new int[] {loop, removed};
+        return new int[] {loop, zero};
     }
 }
