56675d8e39aa28f71e2c00d0e8c19be9094f667a
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-03 19:32:46 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,11 +1,7 @@
 class Solution {
     public int solution(int n) {
-        StringBuilder sb = new StringBuilder();
-        // 10진법을 3진법 수를 저장하는 문자열로 저장
-        String str = Integer.toString(n, 3);
-        // 문자열 뒤집기
-        String reversed = new StringBuilder(str).reverse().toString();
-        // 3진법이 표현된 문자열을 10진법으로 표현
-        return Integer.parseInt(reversed, 3);
+        String s = Integer.toString(n,3);
+        String reversed = new StringBuilder(s).reverse().toString();
+        return Integer.valueOf(reversed, 3);
     }
 }
