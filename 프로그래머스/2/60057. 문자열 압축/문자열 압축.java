2a10acd5c130253c9318ddc6e0bcda19ce04ad8c
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-03 19:26:28 +0900

    [level 2] Title: 문자열 압축, Time: 7.80 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,45 +1,41 @@
 import java.util.*;
-
 class Solution {
     
-    private int compress(String source, int length){
-        StringBuilder sb = new StringBuilder();
-        String last = "";
-        int count = 0;
-        for(String token : split(source, length)){
-            //압축 문자열 구성
-            if(token.equals(last)){
-                count++;
-            } else {
-                if(count > 1){sb.append(count);}
-                    sb.append(last);
-                    last = token;
-                    count = 1;
-            }
-        }
-            if(count > 1) sb.append(count);
-            sb.append(last);   
-        // 압축한 문자열의 길이 반환
-        return sb.length();
-    }
-    
-    private List<String> split(String source, int length){
+    // 문자열 나누기
+    private List<String> split(String s, int n){
         List<String> tokens = new ArrayList<>();
-        // source를 length만큼 잘라서 tokens리스트에 추가
-        for(int i = 0; i < source.length(); i += length){
-            int end = i + length;
-            if(end > source.length()) end = source.length();
-            // 문자열을 i부터 잘라 tokens리스트에 추가
-            tokens.add(source.substring(i,end));
+        for(int i = 0; i < s.length(); i += n){
+            int e = i + n;
+            if(e > s.length()) e = s.length();
+            tokens.add(s.substring(i, e));
         }
         return tokens;
     }
     
+    private int compress(String s, int n){
+        StringBuilder sb = new StringBuilder();
+        String last = "";
+        int count = 0;
+        for(String token : split(s, n)){
+            if(token.equals(last)){
+                count++;
+            } else {
+                if(count > 1) sb.append(count);
+                sb.append(last);
+                last = token;
+                count = 1;
+            }
+        }
+        if(count > 1) sb.append(count);
+        sb.append(last);
+        return sb.length();
+    }
+    
     public int solution(String s) {
+        // 길이 단이 별로 함수 돌려서 최소값 구하기
         int min = Integer.MAX_VALUE;
         for(int i = 1; i <= s.length(); i++){
-            // 문자열 압축 후 가장 짧은 길이 선택
-            int compressed = compress(s, i);
+            int compressed = compress(s,i);
             if(compressed < min){
                 min = compressed;
             }
