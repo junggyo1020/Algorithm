c0eed62b000332f2859c8d5c217262274d84bcde
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-07 16:27:41 +0900

    [D3] Title: N-Queen, Time: 150 ms, Memory: 20,772 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -2,27 +2,28 @@ import java.util.Scanner;
 import java.io.FileInputStream;
 class Solution
 {
-    static int[] board;
-    static int n;
-    static int count;
+    private static int n;
+    private static int[] board;
+    private static int count;
     
     private static void solveQueen(int row){
-    	if(row == n){
+    	// 종료 조건
+        if(row == n){ // n열까지 다 두었다는 것은 경우의 수가 존재한다는 것
         	count++;
-			return;
+            return;
         }
         
         for(int col = 0; col < n; col++){
-        	if(isSafe(row,col)){
-            	board[row] = col;
-                solveQueen(row+1);
+        	if(isSafe(row,col)){ // 해당 열에 퀸을 둘 수 있는지 검사
+            	board[row] = col; // 해당 열에 퀸 두기
+                solveQueen(row+1); // 다음열에 퀸 두기
             }
         }
     }
     
     private static boolean isSafe(int row, int col){
     	for(int i = 0; i < row; i++){
-            if(board[i] == col || Math.abs(board[i] - col) == Math.abs(i - row)) return false;
+        	if(board[i] == col || Math.abs(board[i] - col) == Math.abs(i - row)) return false;
         }
         return true;
     }
@@ -34,12 +35,12 @@ class Solution
 		T=sc.nextInt();
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-            n = sc.nextInt();
-            board = new int[n];
-            count = 0; // 경우의 수
-            solveQueen(0); // 0행 부터 퀸 놓기
+            n = sc.nextInt(); //체스판의 크기
+            board = new int[n]; //Queen의 위치
+			count = 0; //경우의 수
+            solveQueen(0); //퀸 두기
             
-            System.out.printf("#%d %d\n", test_case, count);
+			System.out.printf("#%d %d\n", test_case, count);
 		}
 	}
 }
