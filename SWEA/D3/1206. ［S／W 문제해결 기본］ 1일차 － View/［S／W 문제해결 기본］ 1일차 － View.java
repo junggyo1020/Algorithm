b7166169588e45562302ba6725cfc4af87c42bb9
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-07 14:01:55 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 220 ms, Memory: 35,272 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,33 +1,34 @@
 import java.util.Scanner;
 import java.io.FileInputStream;
-import java.util.*;
-
 class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
 		int T = 10;
-		
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-            int N = sc.nextInt();
-			int[] arr = new int[N+1];
-            int totalBuilding = 0;
-            for(int i = 0; i < N; i++){
-            	arr[i] = sc.nextInt();
+			int n = sc.nextInt();
+            int[] buildings = new int[n];
+            int viewCount = 0;
+                
+            for(int i = 0; i < n; i++){
+            	buildings[i] = sc.nextInt();
             }
-           
-            // 조망권이 확보된 빌딩 구하기
-            for(int i = 2; i < N-2; i++){
-            	if(arr[i-2] < arr[i] && arr[i-1] < arr[i] && arr[i+1] < arr[i] && arr[i+2] < arr[i]){
-                	// 최대값 찾기
-                    int maxValue = Math.max(Math.max(arr[i-2], arr[i-1]), Math.max(arr[i+1], arr[i+2]));
-                    totalBuilding += (arr[i] - maxValue);
+            
+            for(int i = 2; i < n-2; i++){
+            	int currentHeight = buildings[i];
+                int maxNearHeight = Math.max(
+                	Math.max(buildings[i-2], buildings[i-1]),
+                    Math.max(buildings[i+1], buildings[i+2])
+                );
+                
+                if(currentHeight > maxNearHeight){
+                	viewCount += currentHeight - maxNearHeight;
                 }
             }
             
-            System.out.println("#" + test_case + " " + totalBuilding);
+            System.out.printf("#%d %d\n", test_case, viewCount);
 		}
         sc.close();
 	}
