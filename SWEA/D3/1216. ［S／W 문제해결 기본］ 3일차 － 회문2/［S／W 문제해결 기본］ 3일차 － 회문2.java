97a97812a43dff9b6707d108dacbffbdf8cd8fdb
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-10 16:03:32 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 502 ms, Memory: 110,156 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.java
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.java
@@ -1,12 +1,9 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 class Solution
 {
-    static char[][] wordBoard;
-    
     private static boolean isPalindrome(String s){
-    	//회문인지 검사
-		for(int i = 0; i < s.length() / 2; i++){
+    	for(int i = 0; i < s.length() / 2; i++){
         	if(s.charAt(i) != s.charAt(s.length() - i - 1)){
             	return false;
             }
@@ -20,45 +17,46 @@ class Solution
 		for(int test_case = 1; test_case <= 10; test_case++)
 		{
 			int t = Integer.parseInt(br.readLine());
-            StringBuilder[] colArr = new StringBuilder[100];
-            //null로 초기화 될 경우 문자열을 추가할 수 없다.
+
+			StringBuilder[] colArr = new StringBuilder[100]; // 열마다 문자열을 저장하는 배열
             for(int i = 0; i < 100; i++){
             	colArr[i] = new StringBuilder();
             }
             
-            int max = 0;
-            // 행 검사
+            int max = 0; // 팰린드롬의 최장길이
             for(int i = 0; i < 100; i++){
-            	String inputLine = br.readLine();
-                for(int j = 0; j < 100; j++){
-                	colArr[j].append(inputLine.charAt(j));
+            	String rowStr = br.readLine();
+                for(int j = 0; j < 100; j++){ //열 배열 정보도 저장
+                	colArr[j].append(rowStr.charAt(j));
                 }
                 
                 check: for(int len = 100; len > max; len--){
-                	for(int start = 0; start <= 100 - len; start++){
-                    	String rowSubstring = inputLine.substring(start, start+len);
-                        if(isPalindrome(rowSubstring)){
-                        	max = len;
-                            break check;
-                        }
-                    }
-                }
+                                for(int start = 0; start <= 100 - len; start++){
+                                    String rowSubstring = rowStr.substring(start, start + len);
+                                    if(isPalindrome(rowSubstring)){
+                                        max = len;
+                                        break check;
+                                    }
+                                }
+               	 			}
             }
             
+            //열 검사
             for(int i = 0; i < 100; i++){
-            	String colLine = colArr[i].toString();
+            	String colStr = colArr[i].toString();
+                
                 check: for(int len = 100; len > max; len--){
-                	for(int start = 0; start <= 100 - len; start++){
-                    	String colSubstring = colLine.substring(start, start + len);
-                        if(isPalindrome(colSubstring)){
-                        	max = Math.max(max, len);
-                            break check;
-                        }
-                    }
-                }
+                                for(int start = 0; start <= 100 - len; start++){
+                                    String colSubstring = colStr.substring(start, start + len);
+                                    if(isPalindrome(colSubstring)){
+                                        max = len;
+                                        break check;
+                                    }
+                                }
+                			}
             }
-                           
-           System.out.printf("#%d %d\n", t, max);
+            
+            System.out.printf("#%d %d\n", t, max);
 		}
 	}
 }
