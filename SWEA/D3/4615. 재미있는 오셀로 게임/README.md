017ffc98cc0d9200354da0543d456e29f2d6609d
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-10 17:15:52 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 138 ms, Memory: 20,640 KB -BaekjoonHub

---
+++ SWEA/D3/4615. 재미있는 오셀로 게임/README.md
@@ -0,0 +1,15 @@
+# [D3] 재미있는 오셀로 게임 - 4615 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWQmA4uK8ygDFAXj) 
+
+### 성능 요약
+
+메모리: 20,640 KB, 시간: 138 ms, 코드길이: 2,540 Bytes
+
+### 제출 일자
+
+2024-11-10 17:15
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+class Solution
+{
+    static int[] dy = {-1,-1,-1,0,1,1,1,0};
+    static int[] dx = {-1,0,1,1,1,0,-1,-1};
+    static int[][] board;
+    private static void flip(int x, int y, int color, int n){
+    	for(int d = 0; d < 8; d++){
+        	int nx = x + dx[d];
+            int ny = y + dy[d];
+            boolean canFlip = false; // 뒤집을 수 있는지 여부
+            List<int[]> toFlip = new ArrayList<>(); // 뒤집어야 할 노드의 좌표 저장
+            
+            while(nx >= 0 && nx < n && ny >= 0 && ny < n && board[ny][nx] != 0 && board[ny][nx] != color){
+            	toFlip.add(new int[]{ny, nx});
+                nx += dx[d];
+                ny += dy[d];
+            }
+            
+            if(nx >= 0 && nx < n && ny >= 0 && ny < n && board[ny][nx] == color){
+            	canFlip = true;
+            }
+            
+            if(canFlip){
+            	for(int[] pos : toFlip){
+                	board[pos[0]][pos[1]] = color;
+                }
+            }
+        }
+    }
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int test_case = 1; test_case <= T; test_case++){
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            board = new int[n][n];
+            
+            //초기화
+            int mid = n/2;
+            board[mid - 1][mid - 1] = 2;
+            board[mid][mid] = 2;
+            board[mid-1][mid] = 1;
+            board[mid][mid-1] = 1;
+            
+            for(int i = 0; i < m; i++){
+            	st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken()) - 1;
+                int y = Integer.parseInt(st.nextToken()) - 1;
+                int color = Integer.parseInt(st.nextToken());
+                
+                board[y][x] = color;
+                flip(x, y, color, n); // 뒤집기
+            }
+            
+            int b_count = 0, w_count = 0;
+            for(int i = 0 ; i < n; i++){
+            	for(int j = 0; j < n; j++){
+                	if(board[i][j] == 1){
+                    	b_count++;
+                    } else if(board[i][j] == 2){
+                    	w_count++;
+                    }
+                }
+            }
+            
+            System.out.printf("#%d %d %d\n", test_case, b_count, w_count);
+        }
+	}
+}
