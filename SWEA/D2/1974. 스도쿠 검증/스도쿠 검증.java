8a0ad1130dd8d4e66a95377c6c7cd0945cdb2c67
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-17 02:08:17 +0900

    [D2] Title: 스도쿠 검증, Time: 164 ms, Memory: 21,408 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,71 +1,81 @@
 import java.util.Scanner;
-
-class Solution {
+import java.io.FileInputStream;
+class Solution
+{
+	static int[][] arr;
     
     private static boolean isValidate(int[] arr){
-       	boolean[] check = new boolean[10];
+        boolean[] isCheck = new boolean[10];
     	for(int num : arr){
-        	if(num < 1 || num > 9 || check[num]) return false;
-            check[num] = true;
+        	if(num < 1 || num >9 || isCheck[num]) return false;
+            isCheck[num] = true;
         }
         return true;
     }
     
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        
-        for (int test_case = 1; test_case <= T; test_case++) {
-        	int[][] arr = new int[9][9];
-            boolean isValid = true;
-            for(int i = 0; i < 9; i++){
-            	for(int j = 0; j < 9; j++){
-                	arr[i][j] = sc.nextInt();
-                }
-            }
-            
-            //행 검사
-            for(int i = 0; i < 9; i++){
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			boolean isValid = true;
+			arr = new int[9][9];
+			for(int i=0; i < 9; i++) {
+				for(int j=0; j < 9; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			
+			//가로 탐색
+			for(int i=0; i < 9; i++){
             	if(!isValidate(arr[i])){
                 	isValid = false;
                     break;
                 }
             }
-            
-            //열 검사
-            if(isValid){
-            	for(int i = 0; i < 9; i++){
-                	int[] col = new int[9];
-                    for(int j = 0; j < 9; j++){
-                    	col[j] = arr[j][i];
+			
+			//세로 탐색
+			if(isValid){
+                for(int i=0; i < 9; i++){
+                    int[] col = new int[9];
+                    for(int j=0; j < 9; j++){
+                        col[j] = arr[j][i];
                     }
                     if(!isValidate(col)){
-                    	isValid = false;
+                        isValid = false;
                         break;
                     }
                 }
             }
-            
-            //3x3 검사
-            if(isValid){
-                outerloop:
+			
+			//3x3 탐색
+			if(isValid){
+                outerLoop:
             	for(int i = 0; i < 9; i+=3){
                 	for(int j = 0; j < 9; j+=3){
                     	int[] box = new int[9];
                         int index = 0;
-                        for(int y = i; y < i+3; y++){
-                        	for(int x = j; x < j+3; x++){
-                            	box[index++] = arr[y][x];
+                        for(int k = i; k < i + 3; k++){
+                        	for(int l = j; l < j + 3; l++){
+                           		box[index++] = arr[k][l];
                             }
                         }
-                       	if(!isValidate(box)){
+                        if(!isValidate(box)){
                         	isValid = false;
-                            break outerloop;
+                            break outerLoop;
                         }
                     }
-                }
+                }   
             }
-            System.out.printf("#%d %d\n", test_case, isValid ? 1:0);
+			
+			if(isValid) {
+				System.out.printf("#%d %d\n", test_case, 1);
+			} else {
+				System.out.printf("#%d %d\n", test_case, 0);
+			}
 		}
-    }
+		sc.close();
+	}
 }
