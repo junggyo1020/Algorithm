849f7a2623166899cc103f444ccb7eebb7ef4d30
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-27 19:09:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.14 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,21 +1,23 @@
 class Solution {
     public String solution(String s) {
-        StringBuilder sb = new StringBuilder();
         boolean toUpper = true;
+        StringBuilder sb = new StringBuilder();
         
         for(char c : s.toCharArray()){
             if(!Character.isAlphabetic(c)){
-                sb.append(c);
                 toUpper = true;
+                sb.append(c);
+                continue;
             } else {
                 if(toUpper){
                     sb.append(Character.toUpperCase(c));
                 } else {
                     sb.append(Character.toLowerCase(c));
                 }
-                toUpper = !toUpper;
             }
+            toUpper = !toUpper;
         }
+        
         return sb.toString();
     }
 }
