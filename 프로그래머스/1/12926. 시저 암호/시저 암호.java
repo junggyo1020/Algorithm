04f3a96cb63bca6eea26dd7f590f8a81b9317eb9
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-27 19:01:46 +0900

    [level 1] Title: 시저 암호, Time: 2.76 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,22 +2,17 @@ class Solution {
     
     private char push(char c, int n){
         if(!Character.isAlphabetic(c)) return c;
-        
-        int offset = Character.isUpperCase(c) ? 'A' : 'a';
+        char offset = Character.isUpperCase(c) ? 'A' : 'a';
         int position = c - offset;
-        
-        position = (position + n) % ('Z' - 'A' + 1); // 0부터 'Z'-'A' 까지 표현가능
-        
-        return (char)(offset + position);
+        position = (position + n) % 26;
+        return (char)(position + offset);
     }
     
     public String solution(String s, int n) {
-        //알파벳 각각을 담은 배열 만들기
-        char[] alpha = s.toCharArray();
-        StringBuilder str = new StringBuilder();
-        for(char c : alpha){
-            str.append(push(c, n));
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            sb.append(push(c, n));
         }
-        return str.toString();
+        return sb.toString();
     }
 }
