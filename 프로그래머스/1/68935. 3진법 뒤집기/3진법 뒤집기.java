66178dd87f6fb96c71bab49e2cd7e34efd17a7ec
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-11-28 12:16:52 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int n) {
         String s = Integer.toString(n, 3);
-        StringBuilder sb = new StringBuilder(s);
-        int result = Integer.parseInt(sb.reverse().toString(), 3);
+        String reversed = new StringBuilder(s).reverse().toString();
+        int result = Integer.parseInt(reversed, 3);
         return result;
     }
 }
