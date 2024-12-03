648e2b271a1dc5e9a90bc2c8063c91f914bf727a
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-12-03 15:34:55 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 1.38 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,16 +1,17 @@
 import java.util.*;
+
 class Solution {
     
-    private static final String[][] precedences = {
+    private static String[][] precedences = {
         "+-*".split(""),
         "+*-".split(""),
         "-+*".split(""),
         "-*+".split(""),
         "*+-".split(""),
-        "*-+".split(""),
+        "*-+".split("")
     };
     
-    private long calculator(long lhs, long rhs, String op){
+    private long calculate(long lhs, long rhs, String op){
         return switch(op){
                 case "+" -> lhs + rhs;
                 case "-" -> lhs - rhs;
@@ -19,13 +20,13 @@ class Solution {
         };
     }
     
-    private long calculate(List<String> tokens, String[] precedences){
-        for(String op : precedences){
+    private long calculate(List<String> tokens, String[] precedence){
+        for(String op : precedence){
             for(int i = 0; i < tokens.size(); i++){
                 if(tokens.get(i).equals(op)){
                     long lhs = Long.parseLong(tokens.get(i-1));
                     long rhs = Long.parseLong(tokens.get(i+1));
-                    long result = calculator(lhs, rhs, op);
+                    long result = calculate(lhs, rhs, op);
                     tokens.remove(i-1);
                     tokens.remove(i-1);
                     tokens.remove(i-1);
@@ -38,7 +39,8 @@ class Solution {
     }
     
     public long solution(String expression) {
-        StringTokenizer st = new StringTokenizer(expression, "+-*", true); // true는 구분자도 포함하여 토큰화
+        StringTokenizer st = new StringTokenizer(expression, "+-*", true);
+        
         List<String> tokens = new ArrayList<>();
         while(st.hasMoreTokens()){
             tokens.add(st.nextToken());
@@ -47,9 +49,7 @@ class Solution {
         long max = 0;
         for(String[] precedence : precedences){
             long value = Math.abs(calculate(new ArrayList<>(tokens), precedence));
-            if(value > max){
-                max = value;
-            }
+            max = Math.max(max, value);
         }
         return max;
     }
