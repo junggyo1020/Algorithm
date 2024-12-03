a8fd1e71035da94a886ddc4db750323068ea61d3
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-12-03 12:33:25 +0900

    [level 2] Title: 모음 사전, Time: 16.95 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
 class Solution {
+    
     private static final String CHARS = "AEIOU";
     
     private static void generated(String word, List<String> words){
         words.add(word);
         
-        if(word.length() == 5) return;
+        if(word.length() == 5){ return; }
         
         for(char c : CHARS.toCharArray()){
             generated(word + c, words);
         }
     }
-    
+        
     public int solution(String word) {
         List<String> words = new ArrayList<>();
         generated("", words);
