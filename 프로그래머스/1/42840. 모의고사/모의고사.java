48ed19ba342ea2f8ab22bd89f1a5a14817278952
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-12-03 13:21:05 +0900

    [level 1] Title: 모의고사, Time: 7.08 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,32 +1,29 @@
-import java.util.*;
 import java.util.stream.*;
+import java.util.*;
 
 class Solution {
     
     private static final int[][] RULES = {
         {1,2,3,4,5},
         {2,1,2,3,2,4,2,5},
-        {3,3,1,1,2,2,4,4,5,5},
+        {3,3,1,1,2,2,4,4,5,5}
     };
     
-    private int getPicked(int person, int problem){
-        int[] rules = RULES[person];
-        int idx = problem % rules.length;
-        return rules[idx];
+    private static int getPicked(int person, int problem){
+        int[] rule = RULES[person];
+        int index = problem % rule.length;
+        return rule[index];
     }
     
     public int[] solution(int[] answers) {
         int[] corrects = new int[3];
         int max = 0;
-        
         for(int problem = 0; problem < answers.length; problem++){
             int answer = answers[problem];
             for(int person = 0; person < 3; person++){
                 int picked = getPicked(person, problem);
-                if(picked == answer){
-                    if(++corrects[person] > max){
-                        max = corrects[person];
-                    }
+                if(answer == picked){
+                    if(++corrects[person] > max) max = corrects[person];
                 }
             }
         }
@@ -34,7 +31,7 @@ class Solution {
         final int maxCorrects = max;
         return IntStream.range(0,3)
             .filter(i -> corrects[i] == maxCorrects)
-            .map(i -> i+1)
+            .map(i -> i + 1)
             .toArray();
     }
 }
