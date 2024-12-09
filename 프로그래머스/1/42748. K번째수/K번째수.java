cd9d7a7fc6c92a5f1e943abb4b3bf0f989083a90
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-12-09 23:35:38 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        for(int i = 0; i < answer.length; i++){
+        int[] result = new int[commands.length];
+        for(int i = 0; i < result.length; i++){
             int[] command = commands[i];
             int s = command[0] - 1;
             int e = command[1];
@@ -14,8 +14,8 @@ class Solution {
             // 배열을 정렬한다
             Arrays.sort(sub);
             // 배열의 특정 인덱스값을 구한다
-            answer[i] = sub[k]; 
+            result[i] = sub[k]; 
         }
-        return answer;
+        return result;
     }
 }
