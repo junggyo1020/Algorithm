e28b247b356a08cd62358b71dea67ae0920aacae
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-12-24 21:51:11 +0900

    [level 2] Title: 카펫, Time: 14.50 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] result = new int[2];
-        outerLoop:
-        for(int w = 3; w < 5000; w++){
-            for(int h = 1; h < 5000; h++){
-                if(brown == w*h-yellow && yellow == (w-2)*(h-2)){
-                    result[0] = h;
-                    result[1] = w;
-                    break outerLoop;
+        for(int w = 3; w <= 5000; w++){
+            for(int h = 1; h <= 5000; h++){
+                int area = (w-2)*(h-2);
+                if((yellow == area) && (brown == (w*h)-area)){
+                    return new int[]{h,w};
                 }
             }
         }
-        return result;
+        return null;
     }
 }
