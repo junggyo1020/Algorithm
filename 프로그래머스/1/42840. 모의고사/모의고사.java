a76f45cca785b2769a745def61fb261c76191950
Willie <150939763+junggyo1020@users.noreply.github.com>
2024-12-24 21:31:23 +0900

    [level 1] Title: 모의고사, Time: 3.50 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,6 +1,4 @@
-import java.util.stream.*;
 import java.util.*;
-
 class Solution {
     
     private static final int[][] RULES = {
@@ -9,29 +7,31 @@ class Solution {
         {3,3,1,1,2,2,4,4,5,5}
     };
     
-    private static int getPicked(int person, int problem){
-        int[] rule = RULES[person];
-        int index = problem % rule.length;
-        return rule[index];
-    }
-    
     public int[] solution(int[] answers) {
-        int[] corrects = new int[3];
-        int max = 0;
-        for(int problem = 0; problem < answers.length; problem++){
-            int answer = answers[problem];
-            for(int person = 0; person < 3; person++){
-                int picked = getPicked(person, problem);
-                if(answer == picked){
-                    if(++corrects[person] > max) max = corrects[person];
+        List<Integer> results = new ArrayList<>();
+        int[] correct_counts = new int[3];
+        int max_cnt = 0;
+        // 학생별 맞힌 정답 개수 세기
+        for(int student = 0; student < 3; student++){
+            int cnt = 0;
+            int index = RULES[student].length;
+            for(int problem = 0; problem < answers.length; problem++){
+                int answer = RULES[student][problem % index];
+                if(answer == answers[problem]){
+                    cnt++;
                 }
+            }
+            correct_counts[student] = cnt;
+            if(max_cnt < cnt) max_cnt = cnt;
+        }
+        
+        // 최대 맞힌 개수 같은 학생 모두 출력
+        for(int i = 0; i < 3; i++){
+            if(max_cnt == correct_counts[i]){
+                results.add(i);
             }
         }
         
-        final int maxCorrects = max;
-        return IntStream.range(0,3)
-            .filter(i -> corrects[i] == maxCorrects)
-            .map(i -> i + 1)
-            .toArray();
+        return results.stream().mapToInt(i -> i+1).toArray();
     }
 }
