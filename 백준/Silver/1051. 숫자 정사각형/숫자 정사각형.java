b06d374ead465f20c0302bc70829ed4bcdf03f8d
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-05-06 13:13:54 +0900

    [Silver III] Title: 숫자 정사각형, Time: 180 ms, Memory: 17796 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -1,15 +1,6 @@
 import java.util.Scanner;
 
 class Main {
-    static class Point {
-        int y, x;
-
-        Point(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -18,7 +9,7 @@ class Main {
         for (int i = 0; i < n; i++) {
             String s = sc.next();
             for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
+                arr[i][j] = s.charAt(j) - '0';
             }
         }
 
@@ -26,12 +17,8 @@ class Main {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 int tmp = arr[i][j];
-                boolean flag = false;
-                for (int k = 1; k < 50; k++) {
-                    int ny = i + k;
-                    int nx = j + k;
-                    if(ny >= n || nx >= m) break;
-                    if(arr[ny][j] == tmp && arr[i][nx] == tmp && arr[ny][nx] == tmp){
+                for (int k = 1; i + k < n && j + k < m; k++) {
+                    if(arr[i+k][j] == tmp && arr[i][j+k] == tmp && arr[i+k][j+k] == tmp){
                         answer = Math.max(answer, (k+1)*(k+1));
                     } // 같지않으므로 건너뜀}
                 }
