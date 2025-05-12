26d49fc98d26231fb8e36b64ad3e4b88c097ca84
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-05-12 18:13:52 +0900

    [Silver I] Title: 종이 접기, Time: 200 ms, Memory: 18528 KB -BaekjoonHub

--- 백준/Silver/1802. 종이 접기/종이 접기.java
+++ 백준/Silver/1802. 종이 접기/종이 접기.java
@@ -7,26 +7,27 @@ class Main {
         int T = sc.nextInt();
 
         while (T-- > 0) {
-            String str = sc.next();
-            int n = str.length();
-            int[] s = new int[n];
+            String s = sc.next();
+            int n = s.length();
+            int[] arr = new int[n];
             for (int i = 0; i < n; i++) {
-                s[i] = str.charAt(i) - '0';
+                arr[i] = s.charAt(i) - '0';
             }
-            System.out.println(canFold(s, 0, n -1) ? "YES" : "NO");
+
+            System.out.println(canFold(arr, 0, n-1) ? "YES" : "NO");
         }
     }
 
-    private static boolean canFold(int[] s, int left, int right) {
+    private static boolean canFold (int[] arr, int left, int right) {
         int len = right - left + 1;
         int mid = (left + right) / 2;
 
         if(len == 1) return true;
 
         for (int i = 0; i < len / 2; i++) {
-            if(s[left + i] == s[right - i]) return false;
+            if(arr[left + i] == arr[right - i]) return false;
         }
 
-        return canFold(s, left, mid -1) && canFold(s, mid + 1, right);
+        return canFold(arr, left, mid - 1) && canFold(arr, mid + 1, right);
     }
 }
