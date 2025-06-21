f9aa80fa28d6d1ae0025c34fa11d4a463d5b7d1c
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-06-22 04:18:15 +0900

    [Gold II] Title: 색종이 붙이기, Time: 192 ms, Memory: 20852 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -21,37 +21,32 @@ class Main {
         }
 
         //2. 순회
-        dfs(0, 0); //현재 위치, 사용된 색종이 개수
+        dfs(0, 0); //현재 위치, 색종이 최소 개수
 
-        //3. 결과 출력
         System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
     }
 
-    private static void dfs(int pos, int used) {
-        //2-1. 종료 조건
+    private static void dfs(int pos, int cnt) {
         if (pos == 100) {
-            answer = Math.min(answer, used);
+            answer = Math.min(answer, cnt);
             return;
         }
 
         int y = pos / 10;
         int x = pos % 10;
-
-        //0인 칸은 색종이를 붙일 수 없다.
         if (board[y][x] == 0) {
-            dfs(pos + 1, used);
+            dfs(pos + 1, cnt);
             return;
         }
 
-        //2-2. 백트래킹 & 순회
         for (int size = 5; size > 0; size--) {
             if(count[size] == 0) continue;
-            if (canAttach(y, x, size)) { //색종이 붙일 수 있는 경우
-                attach(y, x, size, 0); //색종이 붙이기
+            if (canAttach(y, x, size)) {
+                attach(y, x, size, 0);
                 count[size]--;
-                dfs(pos + 1, used + 1);
+                dfs(pos + 1, cnt + 1);
                 count[size]++;
-                attach(y, x, size, 1); //색종이 다시 떼기
+                attach(y, x, size, 1);
             }
         }
     }
