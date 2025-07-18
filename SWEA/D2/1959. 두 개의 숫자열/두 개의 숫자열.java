bf3594913c2725cb0b69aa4857de1b2420334f8b
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-07-18 15:51:22 +0900

    [D2] Title: 두 개의 숫자열, Time: 148 ms, Memory: 20,384 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -1,94 +1,70 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+/**
+ * @author: SSAFY
+ * @date: 2025. 7. 18.
+ * @link:https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZewY0EKsb3HBIN_&contestProbId=AV5PpoFaAS4DFAUq&probBoxId=AZfDwgOq3cLHBISV&type=PROBLEM&problemBoxTitle=%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98+Track%28%EB%82%9C%EC%9D%B4%EB%8F%84+%EC%A4%91%29&problemBoxCnt=3
+ * @performance:
+ * @category:투포인터, 누적합
+ * @note:
  */
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-		//System.setIn(new FileInputStream("res/input.txt"));
+public class Solution {
 
-		/*
-		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			int N = sc.nextInt();
-            int M = sc.nextInt();
-            int[] A = new int[N];
-            int[] B = new int[M];
-            int maxValue = Integer.MIN_VALUE;
-            
-            for (int i = 0; i < N; i++) {
-                A[i] = sc.nextInt();
-            }
-            for (int i = 0; i < M; i++) {
-                B[i] = sc.nextInt();
-            }
-            
-            if(N<=M){
-            	for(int i = 0; i < M-N+1; i++){
-                   	int sum  = 0;
-                	for(int j = 0; j < N; j++){
-                    	sum += A[j] * B[i+j];
-                    }
-                    maxValue = Math.max(maxValue, sum);
-                }
-            } else {
-            	for(int i = 0; i < N-M+1; i++){
-                    int sum = 0;
-                	for(int j = 0; j < M; j++){
-                    	sum += A[i+j] * B[j];
-                    }
-                    maxValue = Math.max(maxValue, sum);
-                }
-            }
-            System.out.printf("#%d %d\n", test_case, maxValue);
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] arrN, arrM;
+	static int mSum;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			mSum = Integer.MIN_VALUE;
+			
+			//배열 N 입력
+			arrN = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < N; i++) {
+				arrN[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			//배열 M 입력
+			arrM = new int[M];
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < M; i++) {
+				arrM[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			//배열 A의 길이가 긴 경우
+			if(N < M) {
+				int[] tmp = arrN;
+				arrN = arrM;
+				arrM = tmp;
+				
+				int tmp2 = N;
+				N = M;
+				M = tmp2;
+			}
+			
+			for(int rt = 0; rt < N - M + 1; rt++) {
+				int sum = 0; //곱한 합
+				for(int k = 0; k < M; k++) {
+					//배열의 합 구하기
+					sum += arrN[rt+k] * arrM[k];
+				}
+				mSum = Math.max(mSum, sum);
+			}
+			sb.append("#").append(t).append(" ").append(mSum).append('\n');
 		}
-        sc.close();
+		System.out.println(sb);
 	}
+
 }
