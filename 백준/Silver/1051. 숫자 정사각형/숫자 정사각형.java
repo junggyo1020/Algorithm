514343b930f876d5002928f29c4efe68e7baf3f6
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-07-27 14:54:42 +0900

    [Silver III] Title: 숫자 정사각형, Time: 64 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -1,29 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] arr = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            String s = sc.next();
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = s.charAt(j) - '0';
+public class Main {
+
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        //#1 배열 저장
+        int[][] square = new int[n][m];
+        for(int i = 0; i < n; i++){
+            String s = br.readLine();
+            for(int j = 0; j < m; j++){
+                square[i][j] = s.charAt(j) - '0';
             }
         }
 
-        int answer = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int tmp = arr[i][j];
-                for (int k = 1; i + k < n && j + k < m; k++) {
-                    if(arr[i+k][j] == tmp && arr[i][j+k] == tmp && arr[i+k][j+k] == tmp){
-                        answer = Math.max(answer, (k+1)*(k+1));
-                    } // 같지않으므로 건너뜀}
+        int max_len = Math.min(n, m);
+        for(int len = max_len; len >= 1; len--){
+            for(int r = 0; r < n - len + 1; r++){
+                for(int c = 0; c < m - len + 1; c++){
+                    int top_left = square[r][c];
+                    int top_right = square[r][c + len - 1];
+                    int bottom_left= square[r + len - 1][c];
+                    int bottom_right = square[r + len - 1][c + len - 1];
+
+                    if(top_left == top_right && top_left == bottom_left && top_left == bottom_right){
+                        System.out.println(len*len);
+                        return;
+                    }
                 }
             }
         }
-        System.out.println(answer);
+
     }
 }
