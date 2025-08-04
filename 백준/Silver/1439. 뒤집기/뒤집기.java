eb3113402e0307bcb27116a9334fc37cc6c5a42a
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-08-04 13:30:09 +0900

    [Silver V] Title: 뒤집기, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -2,20 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main {
+public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        int zero_cnt = 0, one_cnt = 0, prev = -1;
-        for (char x : s.toCharArray()) {
-            int tmp = x - '0';
-            if (tmp != prev) {
-                if(tmp == 0) zero_cnt++;
-                if(tmp == 1) one_cnt++;
-            }
-            prev = tmp;
-        }
-        System.out.println(Math.min(zero_cnt, one_cnt));
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		
+		//뒤집히는 숫자 개수 세기
+		int one_cnt = 0;
+		int zero_cnt = 0;
+		int prev = S.charAt(0) - '0';
+		
+		//초기 증가
+		if(prev == 1) one_cnt++;
+		else zero_cnt++;
+		
+		for(char c : S.toCharArray()) {
+			int num = c - '0';
+			if(prev != num) {
+				if(num == 1) one_cnt++;
+				else zero_cnt++;
+				prev = num;
+			}
+		}
+		System.out.println(Math.min(zero_cnt, one_cnt));
+	}
+
 }
