5aef395cb9aba0163f3081455b3df3b9e4a528e2
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-08-07 16:04:39 +0900

    [D3] Title: 햄버거 다이어트, Time: 361 ms, Memory: 23,332 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,42 +1,56 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.util.*;
+import java.io.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
 class Solution
 {
-    static int n, calorieLimit;
-    static int[] tasteScores;
-    static int[] calories;
-    static int maxTasteScore;
-    
-    private static void dfs(int index, int currentTaste, int currentCalorie){
-    	if(currentCalorie > calorieLimit) return;
-        maxTasteScore = Math.max(currentTaste, maxTasteScore);
-        if(index == n) return;
-        //해당 재료 포함안하는 경우
-        dfs(index + 1, currentTaste, currentCalorie);
-        //해당 재료 포함하는 경우
-        dfs(index + 1, currentTaste + tasteScores[index], currentCalorie + calories[index]);
-    }
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            n = sc.nextInt();
-            calorieLimit = sc.nextInt();
-            tasteScores = new int[n];
-            calories = new int[n];
-            
-            for(int i = 0; i < n; i++){
-            	tasteScores[i] = sc.nextInt();
-                calories[i] = sc.nextInt();
-            }
-            
-            maxTasteScore = 0;
-            dfs(0,0,0);
-            
-            System.out.printf("#%d %d\n", test_case, maxTasteScore);
+    static int N, L, maxScore;
+	static int[] scores, cals;
+	static boolean[] isSelected;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		//#1. 테스트케이스 만큼 반복
+		for(int t = 1; t <= T; t++) {
+			//#2. 기본 입력
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
+			scores = new int[N];
+			cals = new int[N];
+			isSelected = new boolean[N+1];
+			for(int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				scores[i] = Integer.parseInt(st.nextToken());
+				cals[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			//#3. 조합으로 모든 경우의 수 구하기
+			maxScore = Integer.MIN_VALUE; //가장 맛에 대한 점수가 높은 햄버거 점수
+			dfs(0, 0, 0);
+			
+			//#5. 결과 출력
+			System.out.printf("#%d %d\n", t, maxScore);
 		}
 	}
+	
+	private static void dfs(int cnt, int sum, int cal) {
+		//#4. 칼로리를 넘어서면 종료
+		if(cal > L) {
+			return;
+		} 
+		
+		if(cnt == N) {
+			maxScore = Math.max(maxScore, sum);
+			return;
+		}
+		
+		dfs(cnt+1, sum + scores[cnt], cal + cals[cnt]);
+		dfs(cnt+1, sum, cal);
+    }
 }
