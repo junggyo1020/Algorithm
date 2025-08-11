0dd06aa740c0b380b31931fa1af5f7b5ec8f3e4b
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-08-11 16:19:59 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 572 ms, Memory: 48896 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,44 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
-    static final int MAX = 1000000;
-    static boolean[] isPrime = new boolean[MAX + 1];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sieve();
+	static int n;
+	static boolean[] isNotPrime = new boolean[1000001];
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int i = 2; i <= 1000000; i++) {
+			if(!isNotPrime[i]) {
+				for(long j = (long)i*i; j <= 1000000; j+=i) {
+					isNotPrime[(int)j] = true;
+				}
+			}
+		}
+		
+		String input = null;
+		while(!(input = br.readLine()).equals("0")) {
+			n = Integer.parseInt(input);
+			
+			//하나를 뽑아 다른 하나가 소수인지 확인
+			int max_num = -1;
+			int ans_a = -1;
+			int ans_b = -1;
+			for(int i = 2; i < n; i++) {
+				//a,b 둘다 소수이면
+				if(!isNotPrime[i] && !isNotPrime[n-i]) {
+					if(max_num < n-i-i) {
+						max_num = n - i - i;
+						ans_a = i;
+						ans_b = n-i;
+						break;
+					}
+				}
+			}
+			
+			//결과 출력
+			if(max_num != -1) {
+				sb.append(n).append(" = ").append(ans_a).append(" + ").append(ans_b).append("\n");
+			} else {
+				sb.append("Goldbach's conjecture is wrong.").append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
 
-        while (true) {
-            int n = Integer.parseInt(br.readLine());
-            if (n == 0) break;
-
-            boolean found = false;
-            for (int a = 3; a <= n / 2; a += 2) {
-                if (isPrime[a] && isPrime[n - a]) {
-                    System.out.println(n + " = " + a + " + " + (n - a));
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                System.out.println("Goldbach's conjecture is wrong.");
-            }
-        }
-    }
-
-    static void sieve() {
-        for (int i = 2; i <= MAX; i++) {
-            isPrime[i] = true;
-        }
-        for (int i = 2; i * i <= MAX; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= MAX; j += i) {
-                    isPrime[j] = false;
-                }
-            }
-        }
-    }
 }
