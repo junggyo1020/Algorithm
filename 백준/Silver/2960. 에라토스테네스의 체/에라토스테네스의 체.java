bb81b17737713ba4ff8246cef4f5dc870ec53c76
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-08-11 14:50:10 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 68 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -3,28 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        boolean[] isDeleted = new boolean[n+1];
-        int count = 0;
-
-        for (int i = 2; i <= n; i++) {
-            for (int j = i; j <= n; j += i) {
-                if (!isDeleted[j]) {
-                    isDeleted[j] = true;
-                    count++;
-                    if (count == k) {
-                        System.out.println(j);
-                        return;
-                    }
-                }
-            }
-        }
-    }
+	static int n, k;
+	static boolean[] isRemoved;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		isRemoved = new boolean[n+1];
+		
+		int cnt = 0;
+		for(int i = 2; i <= n; i++) {
+			if(!isRemoved[i]) {
+				for(int j = i; j <= n; j+=i) {
+					if(!isRemoved[j]) {
+						isRemoved[j] = true;
+						++cnt;
+						if(cnt == k) {
+							System.out.println(j);
+							return;
+						}
+					}
+				}
+			}
+		}
+		
+	}
 }
