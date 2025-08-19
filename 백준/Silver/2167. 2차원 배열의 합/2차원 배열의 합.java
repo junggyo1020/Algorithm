d5253c8be11804fd0ceae337505b1f9d2930d617
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-08-19 17:10:47 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 232 ms, Memory: 25772 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -3,39 +3,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
+	
+	static StringBuilder sb = new StringBuilder();
+	static int N, M, K;
+	static int[][] arr;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		long [][] prefixSum = new long[N+1][M+1];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				prefixSum[i+1][j+1] = prefixSum[i][j+1] + prefixSum[i+1][j] - prefixSum[i][j] + arr[i][j];  
+			}
+		}
+		
+		K = Integer.parseInt(br.readLine());
+		for(int k = 0; k < K; k++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			long answer = prefixSum[x][y]-prefixSum[i-1][y]-prefixSum[x][j-1]+prefixSum[i-1][j-1];
+			sb.append(answer).append("\n");
+		}
+		System.out.println(sb);
+	}
 
-    static int n, m, k, a, b, c, d;
-    static int[][] arr;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[n][m];
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        st = new StringTokenizer(br.readLine());
-        k = Integer.parseInt(st.nextToken());
-        for(int i = 0; i < k; i++){
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
-            d = Integer.parseInt(st.nextToken());
-
-            int sum = 0;
-            for(int row = a; row <= c; row++){
-                for(int col = b; col <= d; col++){
-                    sum += arr[row - 1][col - 1];
-                }
-            }
-            System.out.println(sum);
-        }
-    }
 }
