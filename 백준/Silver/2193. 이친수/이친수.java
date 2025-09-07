6e3cbe04b4e35a0ef504e5abb22a78bf6fa06e3b
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-09-07 16:23:05 +0900

    [Silver III] Title: 이친수, Time: 96 ms, Memory: 12856 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
 
-class Main {
+public class Main {
+	/**
+	 * 0으로 시작하지 않는다.
+	 * 1이 두번 연속으로 나타나지 않는다.
+	 * dp[N] : N자리 이진수의 개수
+	 * dp[1] = 1
+	 * dp[2] = 10 = 1
+	 * dp[3] = 100 101 = 2
+	 * dp[4] = 1001 1010 1000 = 3
+	 * dp[5] = 10000 10001 10010 10100 10101 = 5
+	 * dp[6] = 100000 100001 100010 100100 100101 101000 101001 101010 = 8
+	 * dp[N] = dp[N-1] + dp[N-2]
+	 * @param args
+	 */
 
-    static int n;
-    static long[] arr;
+	static long[] dp;
 
-    private static long dp(int num) {
+	private static long dp(int n){
+		if(dp[n] > 0) return dp[n];
+		if(n == 1 || n == 2) return dp[n] = 1;
 
-        if(num == 1 || num == 2) return 1;
-        if(arr[num] != 0) return arr[num];
-        return arr[num] = dp(num-1) + dp(num-2);
-    }
+		return dp[n] = dp(n-1) + dp(n-2);
+	}
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt(); //n자리 이진수 개수 구해야 함.
-        arr = new long[n+1]; //1-based index 이므로 n+1 크기로 지정
-
-        System.out.println(dp(n));
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		dp = new long[n+1];
+		System.out.println(dp(n));
+	}
 }
