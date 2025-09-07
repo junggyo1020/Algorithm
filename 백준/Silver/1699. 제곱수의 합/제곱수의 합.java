d2992fea5b3d642dc0ca029a96d7fbd8b227cecb
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-09-07 17:37:08 +0900

    [Silver II] Title: 제곱수의 합, Time: 120 ms, Memory: 12364 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -1,22 +1,47 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-class Main {
+public class Main {
+	/**
+	 * N이 10만이니까 하향식보다는 상향식 접근이 좋겠다.
+	 * dp[i] = i를 제곱수의 합으로 표현할때 최소 항의 개수
+	 * dp[1] = 1
+	 * dp[2] = 2
+	 * dp[3] = 3
+	 * dp[4] = 1
+	 * dp[5] = dp[4] + dp[1] = 2
+	 * dp[6] = dp[4] + dp[2] = 3
+	 * dp[7] = dp[4] + dp[3] = 4
+	 * dp[8] = dp[4] + dp[4] = 2
+	 * dp[9] = 1
+	 * 
+	 *
+	 * @param args
+	 * @throws IOException
+	 */
 
-    static int n;
-    static int[] arr;
+	static int N;
+	static int[] dp;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        arr = new int[n+1];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		dp = new int[N + 1];
+		Arrays.fill(dp, Integer.MAX_VALUE);
 
-        for (int i = 1; i <= n; i++) {
-            arr[i] = i; //최악의 경우 1^2을 i번 사용함
-            for (int j = 1; j * j <= i; j++) {
-                arr[i] = Math.min(arr[i], arr[i-j*j] + 1);
-            }
-        }
+		for (int i = 1; i * i <= N; i++) {
+			dp[i * i] = 1;
+		}
 
-        System.out.println(arr[n]);
-    }
+		for(int i = 1; i <= N; i++){
+			if(dp[i] == 1) continue;
+			for(int j = 1; j*j < i; j++){
+				dp[i] = Math.min(dp[i], dp[j*j] + dp[i-j*j]);
+			}
+		}
+
+		System.out.println(dp[N]);
+	}
 }
