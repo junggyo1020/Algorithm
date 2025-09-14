a772c0eb478b31d20d75ddfd03ac34a39a205288
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-09-14 22:11:26 +0900

    [D3] Title: 0/1 Knapsack, Time: 108 ms, Memory: 28,288 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -3,42 +3,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 0/1 배낭 문제(Knapsack Problem)
-// 성능: 26,880 kb, 102 ms
 public class Solution {
 
-	static int N, K; // N: 물건의 개수, K: 배낭의 최대 용량
-	static int[] dp; // dp[i][w]: i번째 물건까지 고려하고, 배낭 용량이 w일 때의 최대 가치
-	static int[] maxWeight, maxValue; // 각 물건의 무게(maxWeight)와 가치(maxValue)
+	static int N, K;
+	static int[] weight, value;
+	static int[][] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
-		for(int t = 1; t <= T; t++){
+
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 
-			maxWeight = new int[N+1];
-			maxValue = new int[N+1];
-			dp = new int[K+1];
+			weight = new int[N+1];
+			value = new int[N+1];
+			dp = new int[N+1][K+1];
 
-			for(int i = 1; i <= N; i++){
+			//가방 정보 입력
+			for (int i = 1; i <= N; i++) {
 				st = new StringTokenizer(br.readLine());
-				maxWeight[i] = Integer.parseInt(st.nextToken());
-				maxValue[i] = Integer.parseInt(st.nextToken());
+				weight[i] = Integer.parseInt(st.nextToken());
+				value[i] = Integer.parseInt(st.nextToken());
 			}
 
+			//dp[i][w] : i번째 물건까지 고려할 때 무게 w를 넘지 않는 최대 가치
 			for (int i = 1; i <= N; i++) {
-				for (int j = K; j >= maxWeight[i]; j--) {
-					dp[j] = Math.max(dp[j], maxValue[i] + dp[j-maxWeight[i]]);
+				for (int w = 1; w <= K; w++) {
+					if(w < weight[i]) dp[i][w] = dp[i-1][w];
+					else dp[i][w] = Math.max(dp[i-1][w], value[i] + dp[i-1][w-weight[i]]);
 				}
 			}
 
-			// 최종 결과: N개의 물건을 모두 고려하고, 배낭 용량이 K일 때의 최대 가치
-			sb.append("#").append(t).append(" ").append(dp[K]).append("\n");
-		}
+			sb.append("#").append(t).append(" ").append(dp[N][K]).append("\n");
+		} //end testcase
 		System.out.println(sb);
-	}
+	} //end main
 }
