65dc52f4be584dd0f2b096a9422daa32abdac3e6
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-09-18 09:00:45 +0900

    [Silver IV] Title: 정수 제곱근, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -1,25 +1,33 @@
-import java.math.BigInteger;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BigInteger n = new BigInteger(sc.next());
 
-        BigInteger lt = BigInteger.ZERO;
-        BigInteger rt = new BigInteger("1000000000000000000"); //10^18 -> 2^60
-        BigInteger answer = rt;
-
-        while (lt.compareTo(rt) <= 0) {
-            BigInteger mid = lt.add(rt).divide(BigInteger.TWO);
-            if (mid.multiply(mid).compareTo(n) >= 0) {
-                answer = mid;
-                rt = mid.subtract(BigInteger.ONE);
-            } else {
-                lt = mid.add(BigInteger.ONE);
-            }
-        }
-
-        System.out.println(answer);
-    }
+	static long N;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Long.parseLong(br.readLine());
+		
+		long lo = -1, hi = 4_294_967_296L;
+		
+		while(lo + 1 < hi) {
+			long mid = lo + (hi - lo) / 2;
+			
+			if(isPromising(mid)) { //F F F F T T T T
+				hi = mid;
+			} else {
+				lo = mid;
+			}
+		}
+		
+		System.out.println(hi);
+	}
+	
+	private static boolean isPromising(long q) {
+		if(q == 0) return N==0;
+		if(q > Long.MAX_VALUE/q) return true;
+		return q*q >= N;
+	}
 }
