cdaf526e59f1fa8b6a408a42535b5f72071c093f
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-09-19 21:24:57 +0900

    [Silver III] Title: 선물, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1166. 선물/선물.java
+++ 백준/Silver/1166. 선물/선물.java
@@ -1,34 +1,33 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    class Main {
+public class Main {
 
-        static int N, L, W, H;
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            L = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            H = Integer.parseInt(st.nextToken());
+		double low = 0;
+		double high = Math.min(L,Math.min(W,H));
+		double mid;
 
-            double lt = 0, rt = Math.min(Math.min(L, W), H);
-            double mid = 0;
-            for (int i = 0; i < 40; i++) { // log(10^9) < log(2^30) = 30 -> 40번 이내에 정밀도 1e-9
-                mid = (lt + rt) / 2;
-                if (is_promising(mid)) {
-                    lt = mid;
-                } else {
-                    rt = mid;
-                }
-            }
-            System.out.printf("%.10f\n", mid);
-        }
+		while(low <high){
+			mid = (low+high)/2;
 
-        private static boolean is_promising(double mid) {
-            return (long)(L/mid) * (long)(W/mid) * (long)(H/mid) >= N;
-        }
-    }
+			if((long)(L/mid)*(long)(W/mid)*(long)(H/mid)<N){
+				if(high==mid) break;
+				high = mid;
+			}else{
+				if(low==mid) break;
+				low = mid;
+			}
+		}
+		System.out.println(low);
+	}
+
+}
