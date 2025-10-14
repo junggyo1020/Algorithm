7816ab1ef2223537850b65b7313f1bece78f253a
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-10-14 17:45:51 +0900

    [Gold V] Title: 암호 만들기, Time: 68 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -2,60 +2,69 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
-    private static int L, C;
-    private static char[] alpha, result;
+    static int L, C;
+    static char[] chars;
+    static char[] code;
+    static StringBuilder sb = new StringBuilder();
+    static final Set<Character> vowelsSet = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
-    private static boolean check() {
-        int mChar = 0, jChar = 0;
-        //4-1. 자음 모음 개수 체크
-        for (char x : result) {
-            if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') mChar++;
-            else jChar++;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        chars = new char[C];
+        code = new char[L];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < C; i++) {
+            chars[i] = st.nextToken().charAt(0);
         }
-        return mChar >= 1 && jChar >= 2;
+
+        Arrays.sort(chars);
+
+        dfs(0, 0);
+
+        System.out.print(sb);
     }
-    private static void dfs(int depth, int start) {
-        //3-1. 종료조건
+
+    static void dfs(int start, int depth) {
         if (depth == L) {
-            //4. 조건 맞는지 검증
-            if (check()) {
-                //5. 맞으면 출력
-                StringBuilder sb = new StringBuilder();
-                for (char x : result) {
-                    sb.append(x);
+            if (isValid()) {
+                for (char c : code) {
+                    sb.append(c);
                 }
-                System.out.println(sb);
+                sb.append('\n');
             }
             return;
         }
-        //3-2. 조합 & 백트래킹
+
         for (int i = start; i < C; i++) {
-            result[depth] = alpha[i];
-            dfs(depth + 1, i + 1);
+            code[depth] = chars[i];
+            dfs(i + 1, depth + 1);
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        //1. 입력부
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        L = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        alpha = new char[C];
-        result = new char[L];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < C; i++) {
-            alpha[i] = st.nextToken().charAt(0);
+    static boolean isValid() {
+        int vowels = 0;
+        int consonants = 0;
+
+        for (char c : code) {
+            if (vowelsSet.contains(c)) {
+                vowels++;
+            } else {
+                consonants++;
+            }
         }
 
-        //2. 오름차순 정렬하기
-        Arrays.sort(alpha);
-
-        //3. 조합: C개 중 L개 뽑기
-        dfs(0, 0);
+        return vowels >= 1 && consonants >= 2;
     }
 }
