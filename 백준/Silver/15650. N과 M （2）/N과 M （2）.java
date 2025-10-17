1e4c0e11059485e4a4926f05d3e6537a9ba9840b
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-10-17 14:34:54 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+ *
+ * 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+ * 고른 수열은 오름차순이어야 한다.
+ *
+ * [입력]
+ * 첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+ *
+ * [출력]
+ * 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+ * 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+ */
+public class Main {
+
+    static int N, M;
+    static int[] numbers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[M];
+
+        dfs(0, 1);
+    }
+
+    private static void dfs(int cnt, int start){
+        if(cnt == M){
+            StringBuilder sb = new StringBuilder();
+            for(int x : numbers){
+                sb.append(x).append(" ");
+            }
+            System.out.println(sb);
+            return;
+        }
+
+        for(int i = start; i <= N; i++) {
+            numbers[cnt] = i;
+            dfs(cnt + 1, i + 1);
+        }
+    }
+}
