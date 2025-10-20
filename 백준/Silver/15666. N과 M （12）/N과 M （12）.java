7af000ea4bb7071f0f9d020ccb0f86cf5b29b16f
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-10-20 11:41:32 +0900

    [Silver II] Title: N과 M (12), Time: 96 ms, Memory: 15472 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+/**
+ * 중복조합 : LinkedHashSet + 조합을 구할 수가 주어지는 경우
+ *
+ * 문제
+ * N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+ *
+ * N개의 자연수 중에서 M개를 고른 수열
+ * 같은 수를 여러 번 골라도 된다.
+ * 고른 수열은 비내림차순이어야 한다.
+ * 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+ * 입력
+ * 첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+ *
+ * 둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
+ *
+ * 출력
+ * 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+ *
+ * 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+ */
+public class Main {
+
+    static int N, M;
+    static int[] numbers, results;
+    static HashSet<String> hs;
+    static StringBuilder answer, sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+        results = new int[M];
+        hs = new LinkedHashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+        dfs(0, 0);
+
+        answer = new StringBuilder();
+        for(String s : hs) {
+            answer.append(s).append("\n");
+        }
+        System.out.println(answer);
+    }
+
+    private static void dfs(int cnt, int start) {
+        if(cnt == M) {
+            sb = new StringBuilder();
+            for(int x : results) {
+                sb.append(x).append(" ");
+            }
+            hs.add(sb.toString());
+            return;
+        }
+
+        for(int i = start; i < N; i++) {
+            results[cnt] = numbers[i];
+            dfs(cnt + 1, i);
+        }
+    }
+}
+
