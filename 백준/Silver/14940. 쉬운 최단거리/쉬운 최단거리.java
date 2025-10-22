1e1c75b2bdc1eb6b8757187f8f494e6d6f721e57
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-10-22 16:28:58 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 452 ms, Memory: 46132 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 class Main {
 
     static int n, m, sy, sx;
-    static int[][] map, visited;
+    static int[][] map, dist;
     static final int[] dy = {-1, 1, 0, 0};
     static final int[] dx = {0, 0, -1, 1};
 
@@ -27,26 +27,26 @@ class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         map = new int[n][m];
-        visited = new int[n][m];
+        dist = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                visited[i][j] = -1;
+                dist[i][j] = -1;
 
                 if(map[i][j] == 2) {
                     sy = i;
                     sx = j;
                 }
                 if(map[i][j] == 0) {
-                    visited[i][j] = 0;
+                    dist[i][j] = 0;
                 }
             }
         }
 
         Queue<Point> q = new ArrayDeque<>();
         q.offer(new Point(sy, sx));
-        visited[sy][sx] = 0;
+        dist[sy][sx] = 0;
 
         while(!q.isEmpty()) {
             Point cur = q.poll();
@@ -58,18 +58,17 @@ class Main {
                 int nx = x + dx[i];
 
                 if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-                if(map[ny][nx] == 0) continue;
-                if(visited[ny][nx] != -1) continue;
+                if(dist[ny][nx] != -1) continue;
 
                 q.offer(new Point(ny, nx));
-                visited[ny][nx] = visited[y][x] + 1;
+                dist[ny][nx] = dist[y][x] + 1;
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                sb.append(visited[i][j]).append(" ");
+                sb.append(dist[i][j]).append(" ");
             }
             sb.append("\n");
         }
