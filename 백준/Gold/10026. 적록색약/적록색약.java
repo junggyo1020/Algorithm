25a87f56293c1081372a22847e6fdc8d1f648630
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-10-27 15:37:55 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 12364 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+    static char[][] map;
+    static boolean[][] visited;
+    static final int[] dy = {-1, 1, 0, 0};
+    static final int[] dx = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        for(int i = 0; i < N; i++) {
+            String tmp = br.readLine();
+            for(int j = 0; j < N; j++) {
+                map[i][j] = tmp.charAt(j);
+            }
+        }
+
+        //적록색약이 아닌 사람
+        int cntA = 0;
+        visited = new boolean[N][N];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(!visited[i][j]) {
+                    dfsA(i, j, map[i][j]);
+                    cntA++;
+                }
+            }
+        }
+
+        //적록색약인 사람
+        int cntB = 0;
+        visited = new boolean[N][N];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(!visited[i][j]) {
+                    dfsB(i, j, map[i][j]);
+                    cntB++;
+                }
+            }
+        }
+
+        System.out.println(cntA + " " + cntB);
+    }
+
+    private static void dfsA(int y, int x, char ch) {
+        visited[y][x] = true;
+
+        for(int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if(ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
+            if(!visited[ny][nx] && map[ny][nx] == ch) {
+                visited[ny][nx] = true;
+                dfsA(ny, nx, ch);
+            }
+        }
+    }
+
+    private static void dfsB(int y, int x, char ch) {
+        visited[y][x] = true;
+
+        for(int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if(ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
+            if(visited[ny][nx]) continue;
+            if(ch != 'B' && map[ny][nx] != 'B') {
+                visited[ny][nx] = true;
+                dfsB(ny, nx, ch);
+            }
+            if(ch == 'B' && map[ny][nx] == 'B') {
+                visited[ny][nx] = true;
+                dfsB(ny, nx, ch);
+            }
+        }
+    }
+}
+
