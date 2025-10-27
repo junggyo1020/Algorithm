09933a2aa09ece4589300cd5caf831323f4c0f96
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-10-27 15:41:38 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 12004 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -27,19 +27,26 @@ public class Main {
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
                 if(!visited[i][j]) {
-                    dfsA(i, j, map[i][j]);
+                    dfs(i, j, map[i][j]);
                     cntA++;
                 }
             }
         }
 
         //적록색약인 사람
+        //핵심아이디어 : 맵 자체를 수정하고 dfs 함수를 하나만 쓰자.
         int cntB = 0;
         visited = new boolean[N][N];
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
+                if(map[i][j] == 'G') map[i][j] = 'R';
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
                 if(!visited[i][j]) {
-                    dfsB(i, j, map[i][j]);
+                    dfs(i, j, map[i][j]);
                     cntB++;
                 }
             }
@@ -48,7 +55,7 @@ public class Main {
         System.out.println(cntA + " " + cntB);
     }
 
-    private static void dfsA(int y, int x, char ch) {
+    private static void dfs(int y, int x, char ch) {
         visited[y][x] = true;
 
         for(int i = 0; i < 4; i++) {
@@ -58,29 +65,10 @@ public class Main {
             if(ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
             if(!visited[ny][nx] && map[ny][nx] == ch) {
                 visited[ny][nx] = true;
-                dfsA(ny, nx, ch);
+                dfs(ny, nx, ch);
             }
         }
     }
 
-    private static void dfsB(int y, int x, char ch) {
-        visited[y][x] = true;
-
-        for(int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-
-            if(ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
-            if(visited[ny][nx]) continue;
-            if(ch != 'B' && map[ny][nx] != 'B') {
-                visited[ny][nx] = true;
-                dfsB(ny, nx, ch);
-            }
-            if(ch == 'B' && map[ny][nx] == 'B') {
-                visited[ny][nx] = true;
-                dfsB(ny, nx, ch);
-            }
-        }
-    }
 }
 
