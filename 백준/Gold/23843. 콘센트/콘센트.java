6f379287b40934eb10140f9d3b58ef36cdaeec29
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-10-31 21:22:31 +0900

    [Gold V] Title: 콘센트, Time: 140 ms, Memory: 14928 KB -BaekjoonHub

---
+++ 백준/Gold/23843. 콘센트/콘센트.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		ArrayList<Integer> obj = new ArrayList<>();
+		for(int i = 0; i < N; i++) {
+			obj.add(Integer.parseInt(st.nextToken()));
+		}
+
+		Collections.sort(obj, Collections.reverseOrder()); // 8 4 4 1 1
+
+		//충전에 필요한 시간이 많은 것부터 넣자.
+		PriorityQueue<Long> pq = new PriorityQueue<>();
+		for(int i = 0; i < M; i++) {
+			pq.offer(0L);
+		}
+
+		for(int x : obj) {
+			long time = pq.poll();
+			pq.offer(x + time);
+		}
+
+
+		Long endTime = -1L;
+		for(int i = 0; i < M; i++) {
+			endTime = Math.max(endTime, pq.poll());
+		}
+
+		System.out.println(endTime);
+
+	}
+}
