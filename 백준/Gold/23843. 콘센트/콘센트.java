a439188179700c3ce3635db47ead26163272e882
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-10-31 21:34:13 +0900

    [Gold V] Title: 콘센트, Time: 132 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Gold/23843. 콘센트/콘센트.java
+++ 백준/Gold/23843. 콘센트/콘센트.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -26,19 +27,23 @@ public class Main {
 		//충전에 필요한 시간이 많은 것부터 넣자.
 		PriorityQueue<Long> pq = new PriorityQueue<>();
 		for(int i = 0; i < M; i++) {
-			pq.offer(0L);
+			pq.offer(0L); //끝나는 시간
 		}
 
 		for(int x : obj) {
-			long time = pq.poll();
+			long time = pq.poll(); //콘센트 중 가장 빨리 사용가능한 시간
 			pq.offer(x + time);
 		}
 
-
 		Long endTime = -1L;
-		for(int i = 0; i < M; i++) {
-			endTime = Math.max(endTime, pq.poll());
+		while(!pq.isEmpty()) {
+			endTime = pq.poll();
 		}
+		//
+		// Long endTime = -1L;
+		// for(int i = 0; i < M; i++) {
+		// 	endTime = Math.max(endTime, pq.poll());
+		// }
 
 		System.out.println(endTime);
 
