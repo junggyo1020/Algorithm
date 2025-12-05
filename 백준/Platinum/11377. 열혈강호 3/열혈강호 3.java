768fa8d7a99f6676cb3a7e6e8310281bfd526747
Willie <150939763+junggyo1020@users.noreply.github.com>
2025-12-05 17:12:17 +0900

    [Platinum III] Title: 열혈강호 3, Time: 2556 ms, Memory: 56184 KB -BaekjoonHub

---
+++ 백준/Platinum/11377. 열혈강호 3/열혈강호 3.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M, K;
+	static ArrayList<Integer>[] adj;
+	static int[] match;
+	static boolean[] visited;
+
+	private static boolean dfs(int u) {
+		for(int v : adj[u]) {
+			if(visited[v]) continue;
+			visited[v] = true;
+
+			if(match[v] == 0 || dfs(match[v])) {
+				match[v] = u;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		adj = new ArrayList[N+1];
+		for(int i = 1; i <= N; i++) {
+			adj[i] = new ArrayList<>();
+		}
+
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int k = Integer.parseInt(st.nextToken());
+			for(int j = 0; j < k; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				adj[i].add(num);
+			}
+		}
+
+		int totalMatching = 0;
+		match = new int[M+1];
+
+		for(int i = 1; i <= N; i++) {
+			visited = new boolean[M+1];
+			if(dfs(i)) {
+				totalMatching++;
+			}
+		}
+
+		int cnt = 0;
+		for(int i = 1; i <= N; i++) {
+			if(cnt >= K) break;
+
+			visited = new boolean[M+1];
+			if(dfs(i)) {
+				totalMatching++;
+				cnt++;
+			}
+		}
+
+		System.out.println(totalMatching);
+	}
+}
