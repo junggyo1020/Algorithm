2058c9d83ac0abd5c5b6ce70229ef12c215b92cd
Willie <150939763+junggyo1020@users.noreply.github.com>
2026-01-21 09:46:13 +0900

    [Gold V] Title: 회의실 배정, Time: 344 ms, Memory: 41496 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,53 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 
-    static int N, answer;
+    static int N;
     static StringTokenizer st;
-    static List<Room> rooms;
+    static PriorityQueue<Node> pq1;
 
-    static class Room implements Comparable<Room>{
-        int start, end;
-        Room(int start, int end) {
-            this.start = start;
-            this.end = end;
+    static class Node implements Comparable<Node> {
+        int s, e;
+
+        Node(int s, int e) {
+            this.s = s;
+            this.e = e;
         }
 
         @Override
-        public int compareTo(Room o) {
-            if(this.end == o.end) return Integer.compare(this.start, o.start);
-            return Integer.compare(this.end, o.end);
+        public int compareTo(Node o) {
+            if(this.e == o.e) return Integer.compare(this.s, o.s);
+            return Integer.compare(this.e, o.e);
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        rooms = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
+        pq1 = new PriorityQueue<>();
+        int end = 0;
+        int cnt = 0;
+
+        for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            rooms.add(new Room(start, end));
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            pq1.offer(new Node(s,e));
         }
 
-        Collections.sort(rooms);
+        while(!pq1.isEmpty()) {
+            Node cur = pq1.poll();
 
-        int lastEnd = 0; //가장 최근에 끝난 회의의 시간
-        for(int i = 0; i < rooms.size(); i++) {
-            //다음 회의의 시작시간이 지난 회의가 끝난 시간인 경우
-            if(rooms.get(i).start >= lastEnd) {
-                lastEnd = rooms.get(i).end;
-                answer++;
+            if(end <= cur.s) {
+                end = cur.e;
+                ++cnt;
             }
         }
 
-        System.out.println(answer);
+        System.out.println(cnt);
     }
 }
