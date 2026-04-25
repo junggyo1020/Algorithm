02320e4bfee9ed54b136f74c91c0e1d2da441734
Willie <150939763+junggyo1020@users.noreply.github.com>
2026-04-25 18:03:09 +0900

    [level 2] Title: 타겟 넘버, Time: 5.06 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,50 +1,19 @@
-import java.util.*;
-
 class Solution {
     
-    static List<Integer> list;
-    static int answer, n, result;
-    
-    private static boolean calculate(List<Integer> op, int[] numbers) {
-        int sum = 0;
-        for(int i = 0; i < n; i++) {
-            if(op.get(i) == 1) sum += numbers[i];
-            else sum -= numbers[i];
-        }
-        
-        return sum == result;
-    }
-    
-    private static void dfs(int depth, List<Integer> op, int[] numbers) {
-        //종료조건을 먼저 정의합시다
-        if(depth == n) {
-            //계산을 해보겠습니다.
-            if(calculate(op, numbers)) answer++;
+    static int answer;
+    private static void dfs(int[] numbers, int target, int depth, int current) {
+        if(depth == numbers.length) {
+            if(current == target) answer++;
             return;
         }
         
-        op.add(1);
-        dfs(depth + 1, op, numbers);
-        op.remove(op.size() - 1);
-        op.add(0);
-        dfs(depth + 1, op, numbers);
-        op.remove(op.size() - 1);
+        dfs(numbers, target, depth + 1, current + numbers[depth]);
+        dfs(numbers, target, depth + 1, current - numbers[depth]);
     }
     
     public int solution(int[] numbers, int target) {
-        answer = 0; //타겟 넘버를 만드는 방법의 수
-        
-        //먼저, 주어지는 숫자의 개수를 저장하는 변수를 선언합시다.
-        n = numbers.length;
-        result = target;
-        
-        //이제부터 부호를 저장할 리스트를 선언합니다.
-        list = new ArrayList<>();
-        
-        //우리는 target의 숫자를 맞추기 위해, n개의 부호 조합을 맞춰볼겁니다.
-        //초기값을 설정해야합니다.
-        dfs(0, list, numbers);
-        
+        answer = 0;
+        dfs(numbers, target, 0, 0);
         return answer;
     }
 }
