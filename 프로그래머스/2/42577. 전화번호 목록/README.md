bec12c20d355aed811e6412df654c9557c3146cb
Willie <150939763+junggyo1020@users.noreply.github.com>
2026-07-17 14:03:40 +0900

    [level 2] Title: 전화번호 목록, Time: 216.62 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 198 MB, 시간: 318.97 ms
+메모리: 119 MB, 시간: 216.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 24일 11:52:30
+2026년 07월 17일 14:03:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,22 +1,14 @@
-import java.util.HashMap;
-
+import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
-        HashMap<String, Integer> map = new HashMap<>();
+        Arrays.sort(phone_book);
         
-        for(String phone : phone_book){
-            map.put(phone, 1);
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) return false;
         }
         
-        for(String phone : phone_book){
-            for(int i = 1; i < phone.length(); i++){
-                if(map.containsKey(phone.substring(0,i))){
-                    answer = false;
-                }
-            }
-        }
         
         return answer;
     }
